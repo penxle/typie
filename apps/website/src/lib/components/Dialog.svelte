d8fdb34aa5abd004285f4c52c1f6a865d4d34ff0
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2025-03-17 09:07:19 +0000

    Dialog 추가 (#11)

---
+++ apps/website/src/lib/components/Dialog.svelte
@@ -0,0 +1,105 @@
+<script lang="ts">
+  import { css } from '$styled-system/css';
+  import type { Snippet } from 'svelte';
+  import type { HTMLDialogAttributes } from 'svelte/elements';
+
+  type Props = {
+    open: boolean;
+    children: Snippet;
+  } & HTMLDialogAttributes;
+
+  let { open = $bindable(), children, ...rest }: Props = $props();
+
+  let dialogEl: HTMLDialogElement;
+  let showModal = (dialog: HTMLDialogElement) => dialog.showModal();
+
+  $effect(() => {
+    if (dialogEl)
+      if (open) {
+        dialogEl.showModal();
+        document.body.style.overflow = 'hidden';
+      } else {
+        dialogEl.close();
+        document.body.style.overflow = '';
+      }
+  });
+</script>
+
+<dialog
+  bind:this={dialogEl}
+  class={css({
+    width: 'full',
+    height: 'full',
+    maxWidth: '[unset]',
+    maxHeight: '[unset]',
+    '& ::backdrop': {
+      display: 'none',
+    },
+  })}
+  onclick={(e) => {
+    if (e.target === dialogEl) {
+      dialogEl.close();
+      open = false;
+    }
+  }}
+  onsubmit={(e) => {
+    e.preventDefault();
+    dialogEl.close();
+    open = false;
+  }}
+  use:showModal
+  {...rest}
+>
+  <div
+    class={css({ position: 'absolute', inset: '0', backgroundColor: 'gray.900/24' })}
+    onclick={() => {
+      open = false;
+    }}
+    onkeypress={null}
+    role="button"
+    tabindex="-1"
+  ></div>
+
+  <div
+    class={css({
+      position: 'absolute',
+      inset: '0',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: '40px',
+      width: '[fit-content]',
+      margin: 'auto',
+      pointerEvents: 'none',
+    })}
+  >
+    <div
+      class={css({
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        flexGrow: '1',
+        borderWidth: '1px',
+        borderRadius: '[20px]',
+        backgroundColor: 'white',
+        padding: '20px',
+        pointerEvents: 'auto',
+        size: '[fit-content]',
+        minWidth: '400px',
+        maxWidth: '946px',
+        maxHeight: '738px',
+        overflow: 'hidden',
+      })}
+    >
+      <div class={css({ textAlign: 'right' })}>
+        <button onclick={() => (open = false)} type="button">X</button>
+      </div>
+
+      <div class={css({ height: 'full', overflowY: 'auto' })}>
+        <section class={css({ display: 'contents' })}>
+          {@render children()}
+        </section>
+      </div>
+    </div>
+  </div>
+</dialog>
