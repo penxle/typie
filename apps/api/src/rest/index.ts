e570e379b5b22fa1d8ca3aa952fff52c1f9009db
devunt <337728+devunt@users.noreply.github.com>
2025-03-17 16:11:27 +0000

    모바일 결제시 404 뜨는 문제 해결 (#42)

--- apps/api/src/rest/index.ts
+++ apps/api/src/rest/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono';
 import { healthz } from './healthz';
+import { payment } from './payment';
 
 export const rest = new Hono();
 
 rest.route('/healthz', healthz);
+rest.route('/payment', payment);
