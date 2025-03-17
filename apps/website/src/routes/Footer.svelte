7fd2ec73eac8aeab802995d55858ecf01dc0d1fa
robin-maki <8201350+robin-maki@users.noreply.github.com>
2025-03-17 13:58:03 +0000

    푸터 법적 필수사항 반영 (#16)

--- apps/website/src/routes/Footer.svelte
+++ apps/website/src/routes/Footer.svelte
@@ -14,6 +14,7 @@
       <div>대표 : 배준현</div>
       <div>개인정보관리책임자 : 배준현</div>
       <div>사업자등록번호 : 610-88-03078</div>
+      <div>통신판매업 신고번호 : 2023-서울강남-4541</div>
       <div>주소 : 서울특별시 강남구 강남대로100길 14, 6층</div>
       <div>전화 : 02-565-7695</div>
     </div>
@@ -21,7 +22,7 @@
   <div class={flex({ direction: 'column', gap: '8px' })}>
     <div class={css({ color: '[#A8A8A8]' })}>All rights reserved ⓒ 2025 PENXLE COMPANY</div>
     <div class={css({ color: '[#636363]' })}>
-      <a class={css({ color: '[#3260E1]' })} href="https://glitter.rdbl.io/legal/privacy">개인정보처리방침</a>
+      <a class={css({ fontWeight: '[500]', color: '[#3260E1]' })} href="https://glitter.rdbl.io/legal/privacy">개인정보처리방침</a>
       |
       <a class={css({ color: '[#3260E1]' })} href="https://glitter.rdbl.io/legal/terms">이용약관</a>
     </div>
