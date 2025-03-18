bc2c011307146176472249061502e257f19ebbc8
devunt <337728+devunt@users.noreply.github.com>
2025-03-18 03:09:24 +0000

    이메일 도메인 변경 (#50)

--- apps/infrastructure/pulumi/aws/ses.ts
+++ apps/infrastructure/pulumi/aws/ses.ts
@@ -3,52 +3,52 @@ import { zones } from '$aws/route53';
 
 // spell-checker:words sesv2
 
-export const configurationSet = new aws.sesv2.ConfigurationSet('glitter.im', {
-  configurationSetName: 'glitter_im',
+export const configurationSet = new aws.sesv2.ConfigurationSet('glttr.io', {
+  configurationSetName: 'glttr_io',
 });
 
-export const emailIdentity = new aws.sesv2.EmailIdentity('glitter.im', {
-  emailIdentity: 'glitter.im',
+export const emailIdentity = new aws.sesv2.EmailIdentity('glttr.io', {
+  emailIdentity: 'glttr.io',
   configurationSetName: configurationSet.configurationSetName,
 });
 
-new aws.sesv2.EmailIdentityMailFromAttributes('glitter.im', {
+new aws.sesv2.EmailIdentityMailFromAttributes('glttr.io', {
   emailIdentity: emailIdentity.id,
-  mailFromDomain: 'mail.glitter.im',
+  mailFromDomain: 'mail.glttr.io',
 });
 
 emailIdentity.dkimSigningAttributes.tokens.apply((tokens) => {
   for (const token of tokens) {
-    new aws.route53.Record(`${token}._domainkey.glitter.im`, {
-      zoneId: zones.glitter_im.zoneId,
+    new aws.route53.Record(`${token}._domainkey.glttr.io`, {
+      zoneId: zones.glttr_io.zoneId,
       type: 'CNAME',
-      name: `${token}._domainkey.glitter.im`,
+      name: `${token}._domainkey.glttr.io`,
       records: [`${token}.dkim.amazonses.com`],
       ttl: 300,
     });
   }
 });
 
-new aws.route53.Record('mail.glitter.im|mx', {
-  zoneId: zones.glitter_im.zoneId,
+new aws.route53.Record('mail.glttr.io|mx', {
+  zoneId: zones.glttr_io.zoneId,
   type: 'MX',
-  name: 'mail.glitter.im',
+  name: 'mail.glttr.io',
   records: ['10 feedback-smtp.ap-northeast-2.amazonses.com'],
   ttl: 300,
 });
 
-new aws.route53.Record('mail.glitter.im|txt', {
-  zoneId: zones.glitter_im.zoneId,
+new aws.route53.Record('mail.glttr.io|txt', {
+  zoneId: zones.glttr_io.zoneId,
   type: 'TXT',
-  name: 'mail.glitter.im',
+  name: 'mail.glttr.io',
   records: ['v=spf1 include:amazonses.com ~all'],
   ttl: 300,
 });
 
-new aws.route53.Record('_dmarc.glitter.im|txt', {
-  zoneId: zones.glitter_im.zoneId,
+new aws.route53.Record('_dmarc.glttr.io|txt', {
+  zoneId: zones.glttr_io.zoneId,
   type: 'TXT',
-  name: '_dmarc.glitter.im',
+  name: '_dmarc.glttr.io',
   records: ['v=DMARC1; p=none;'],
   ttl: 300,
 });
