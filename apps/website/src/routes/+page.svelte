5db78f016d5d0361f44eb5778b6ff4993a6029e4
devunt <337728+devunt@users.noreply.github.com>
2025-03-18 04:07:10 +0000

    랜딩 페이지 타이틀 변경 (#52)
    글리터 이름이 앞에 오도록 함

--- apps/website/src/routes/+page.svelte
+++ apps/website/src/routes/+page.svelte
@@ -30,7 +30,7 @@
 <Helmet
   description="창작자가 기다려온 글쓰기 앱을 만나보세요. 지금 얼리버드 한정 특별가로 미리 등록할 수 있어요!"
   image={{ src: 'https://cdn.glttr.io/opengraph/default.png', size: 'large' }}
-  title="몰입해서 쓰고, 유연하게 공유하고, 깔끔하게 정리하는 스마트한 에디터, 글리터"
+  title="글리터 — 몰입해서 쓰고, 유연하게 공유하고, 깔끔하게 정리하는 스마트한 에디터"
   trailing={null}
 />
 
