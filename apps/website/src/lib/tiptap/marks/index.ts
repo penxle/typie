2fe2521e4f5d6f3c43bda7cc8c69a0ee72f5b633
devunt <337728+devunt@users.noreply.github.com>
2025-03-23 10:32:29 +0000

    팁탭 링크 마크 구현 (#82)

--- apps/website/src/lib/tiptap/marks/index.ts
+++ apps/website/src/lib/tiptap/marks/index.ts
@@ -3,6 +3,7 @@ export * from './font-color';
 export * from './font-family';
 export * from './font-size';
 export * from './italic';
+export * from './link';
 export * from './ruby';
 export * from './strike';
 export * from './underline';
