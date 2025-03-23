f03450eb5ea73aaa3e58108bb5d1170a9d02400a
devunt <337728+devunt@users.noreply.github.com>
2025-03-23 10:32:29 +0000

    팁탭 툴바 목업에 이것 저것 더 추가 (#84)

--- apps/website/src/routes/editor/Toolbar.svelte
+++ apps/website/src/routes/editor/Toolbar.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import BoldIcon from '~icons/lucide/bold';
+  import GemIcon from '~icons/lucide/gem';
   import ImageIcon from '~icons/lucide/image';
   import ItalicIcon from '~icons/lucide/italic';
+  import LinkIcon from '~icons/lucide/link';
   import ListIcon from '~icons/lucide/list';
   import ListOrderedIcon from '~icons/lucide/list-ordered';
   import MinusIcon from '~icons/lucide/minus';
@@ -10,6 +12,7 @@
   import TextQuoteIcon from '~icons/lucide/text-quote';
   import UnderlineIcon from '~icons/lucide/underline';
   import { Icon } from '$lib/components';
+  import { defaultValues, values } from '$lib/tiptap/values';
   import { css } from '$styled-system/css';
   import { flex } from '$styled-system/patterns';
   import type { Editor } from '@tiptap/core';
@@ -49,6 +52,34 @@
       icon: StrikethroughIcon,
       active: editor.current.isActive('strike'),
       onclick: () => editor.current.chain().focus().toggleStrike().run(),
+    },
+    {
+      id: 'ruby',
+      label: '루비',
+      icon: GemIcon,
+      active: editor.current.isActive('ruby'),
+      onclick: () => {
+        const text = prompt('루비를 입력하세요', editor.current.getAttributes('ruby').text);
+        if (text) {
+          editor.current.chain().focus().setRuby(text).run();
+        } else {
+          editor.current.chain().focus().unsetRuby().run();
+        }
+      },
+    },
+    {
+      id: 'link',
+      label: '링크',
+      icon: LinkIcon,
+      active: editor.current.isActive('link'),
+      onclick: () => {
+        const url = prompt('링크를 입력하세요', editor.current.getAttributes('link').href);
+        if (url) {
+          editor.current.chain().focus().setLink(url).run();
+        } else {
+          editor.current.chain().focus().unsetLink().run();
+        }
+      },
     },
     {
       id: 'bullet_list',
@@ -95,7 +126,7 @@
   ]);
 </script>
 
-<div class={flex({ alignItems: 'center', gap: '4px' })}>
+<div class={flex({ justifyContent: 'center', alignItems: 'center', gap: '4px', flexWrap: 'wrap', width: 'full', maxWidth: '1000px' })}>
   {#each items as { id, label, icon, active, onclick } (id)}
     <button
       class={flex({
@@ -114,4 +145,138 @@
       <span class={css({ fontSize: '14px' })}>{label}</span>
     </button>
   {/each}
+
+  <select
+    class={css({ borderWidth: '1px', borderRadius: '4px', paddingX: '4px', paddingY: '2px', fontSize: '14px' })}
+    onchange={({ currentTarget }) =>
+      editor.current
+        .chain()
+        .focus()
+        .setFontFamily(currentTarget.value as never)
+        .run()}
+  >
+    {#each values.fontFamily as { label, value } (value)}
+      <option selected={(editor.current.getAttributes('font_family').value ?? defaultValues.fontFamily) === value} {value}>
+        {label}
+      </option>
+    {/each}
+  </select>
+
+  <select
+    class={css({ borderWidth: '1px', borderRadius: '4px', paddingX: '4px', paddingY: '2px', fontSize: '14px' })}
+    onchange={({ currentTarget }) =>
+      editor.current
+        .chain()
+        .focus()
+        .setFontSize(Number(currentTarget.value) as never)
+        .run()}
+  >
+    {#each values.fontSize as { label, value } (value)}
+      <option selected={(editor.current.getAttributes('font_size').value ?? defaultValues.fontSize) === value} {value}>
+        {label}
+      </option>
+    {/each}
+  </select>
+
+  <select
+    class={css({ borderWidth: '1px', borderRadius: '4px', paddingX: '4px', paddingY: '2px', fontSize: '14px' })}
+    onchange={({ currentTarget }) =>
+      editor.current
+        .chain()
+        .focus()
+        .setParagraphLineHeight(Number(currentTarget.value) as never)
+        .run()}
+  >
+    {#each values.lineHeight as { label, value } (value)}
+      <option selected={(editor.current.getAttributes('paragraph').lineHeight ?? defaultValues.lineHeight) === value} {value}>
+        {label}
+      </option>
+    {/each}
+  </select>
+
+  <select
+    class={css({ borderWidth: '1px', borderRadius: '4px', paddingX: '4px', paddingY: '2px', fontSize: '14px' })}
+    onchange={({ currentTarget }) =>
+      editor.current
+        .chain()
+        .focus()
+        .setParagraphLetterSpacing(Number(currentTarget.value) as never)
+        .run()}
+  >
+    {#each values.letterSpacing as { label, value } (value)}
+      <option selected={(editor.current.getAttributes('paragraph').letterSpacing ?? defaultValues.letterSpacing) === value} {value}>
+        {label}
+      </option>
+    {/each}
+  </select>
+
+  <select
+    class={css({ borderWidth: '1px', borderRadius: '4px', paddingX: '4px', paddingY: '2px', fontSize: '14px' })}
+    onchange={({ currentTarget }) =>
+      editor.current
+        .chain()
+        .focus()
+        .setParagraphTextAlign(currentTarget.value as never)
+        .run()}
+  >
+    {#each values.textAlign as { label, value } (value)}
+      <option selected={(editor.current.getAttributes('paragraph').textAlign ?? defaultValues.textAlign) === value} {value}>
+        {label}
+      </option>
+    {/each}
+  </select>
+
+  <select
+    class={css({ borderWidth: '1px', borderRadius: '4px', paddingX: '4px', paddingY: '2px', fontSize: '14px' })}
+    onchange={({ currentTarget }) =>
+      editor.current
+        .chain()
+        .focus()
+        .setBodyMaxWidth(Number(currentTarget.value) as never)
+        .run()}
+  >
+    {#each values.maxWidth as { label, value } (value)}
+      <option selected={(editor.current.getAttributes('body').maxWidth ?? defaultValues.maxWidth) === value} {value}>
+        {label}
+      </option>
+    {/each}
+  </select>
+
+  <select
+    class={css({ borderWidth: '1px', borderRadius: '4px', paddingX: '4px', paddingY: '2px', fontSize: '14px' })}
+    onchange={({ currentTarget }) =>
+      editor.current
+        .chain()
+        .focus()
+        .setBodyBlockGap(Number(currentTarget.value) as never)
+        .run()}
+  >
+    {#each values.blockGap as { label, value } (value)}
+      <option selected={(editor.current.getAttributes('body').blockGap ?? defaultValues.blockGap) === value} {value}>
+        {label}
+      </option>
+    {/each}
+  </select>
+
+  <select
+    class={css({ borderWidth: '1px', borderRadius: '4px', paddingX: '4px', paddingY: '2px', fontSize: '14px' })}
+    onchange={({ currentTarget }) =>
+      editor.current
+        .chain()
+        .focus()
+        .setBodyParagraphIndent(Number(currentTarget.value) as never)
+        .run()}
+  >
+    {#each values.paragraphIndent as { label, value } (value)}
+      <option selected={(editor.current.getAttributes('body').paragraphIndent ?? defaultValues.paragraphIndent) === value} {value}>
+        {label}
+      </option>
+    {/each}
+  </select>
+
+  <input
+    class={css({ borderWidth: '1px', borderRadius: '4px', paddingX: '4px', paddingY: '2px', fontSize: '14px' })}
+    onchange={({ currentTarget }) => editor.current.chain().focus().setFontColor(currentTarget.value).run()}
+    type="color"
+  />
 </div>
