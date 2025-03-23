b7898923cfd85ba992ae459a74971138b83f12f3
devunt <337728+devunt@users.noreply.github.com>
2025-03-23 03:18:18 +0000

    팁탭 bubble menu 구현 (#74)

--- apps/website/src/lib/tiptap/schema.ts
+++ apps/website/src/lib/tiptap/schema.ts
@@ -1,6 +1,6 @@
 import { Behavior, BlockSelectionExt, DropCursor, Placeholder, Typography } from './extensions';
 import { Bold, FontColor, FontFamily, FontSize, Italic, Ruby, Strike, Underline } from './marks';
-import { FloatingMenu, SlashMenu } from './menus';
+import { BubbleMenu, FloatingMenu, SlashMenu } from './menus';
 import { Body, BulletList, Doc, HardBreak, ListItem, OrderedList, Paragraph, Text } from './nodes';
 
 export const extensions = [
@@ -34,6 +34,7 @@ export const extensions = [
   Typography,
 
   // menus
-  SlashMenu,
+  BubbleMenu,
   FloatingMenu,
+  SlashMenu,
 ];
