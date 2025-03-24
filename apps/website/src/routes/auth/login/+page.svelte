c5b3c6746107c43ce756e3d183467c8e79614796
devunt <337728+devunt@users.noreply.github.com>
2025-03-24 02:08:26 +0000

    프론트에 구글 SSO 붙임 (#91)

---
+++ apps/website/src/routes/auth/login/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { SingleSignOnProvider } from '@/enums';
+  import { graphql } from '$graphql';
+  import { center } from '$styled-system/patterns';
+
+  const generateSingleSignOnAuthorizationUrl = graphql(`
+    mutation LoginPage_GenerateSingleSignOnAuthorizationUrl_Mutation($input: GenerateSingleSignOnAuthorizationUrlInput!) {
+      generateSingleSignOnAuthorizationUrl(input: $input)
+    }
+  `);
+</script>
+
+<div class={center({ width: 'screen', height: 'screen' })}>
+  <button
+    onclick={async () => {
+      const url = await generateSingleSignOnAuthorizationUrl({
+        provider: SingleSignOnProvider.GOOGLE,
+      });
+
+      location.href = url;
+    }}
+    type="button"
+  >
+    구글로 시작하기
+  </button>
+</div>
