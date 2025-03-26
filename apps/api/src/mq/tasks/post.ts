fe1a13ad970a5567f8d03309321176fce68fcdda
devunt <337728+devunt@users.noreply.github.com>
2025-03-26 04:52:59 +0000

    포스트 yjs 동기화 redis 이용함 (#104)
    db read/write 비용 감소를 위해 redis 이용하도록 함
    
    안정성은 충분히 검증된 듯.

--- apps/api/src/mq/tasks/post.ts
+++ apps/api/src/mq/tasks/post.ts
@@ -1,7 +1,9 @@
 import dayjs from 'dayjs';
-import { and, desc, eq, gt } from 'drizzle-orm';
+import { and, eq } from 'drizzle-orm';
+import { base64 } from 'rfc4648';
 import * as Y from 'yjs';
-import { db, firstOrThrow, PostContentStates, PostContentUpdates } from '@/db';
+import { redis } from '@/cache';
+import { db, firstOrThrow, PostContentStates } from '@/db';
 import { defineJob } from '../types';
 
 export const PostContentStateUpdateJob = defineJob('post:content:state-update', async (postId: string) => {
@@ -10,24 +12,19 @@ export const PostContentStateUpdateJob = defineJob('post:content:state-update', 
       .select({
         update: PostContentStates.update,
         vector: PostContentStates.vector,
-        seq: PostContentStates.seq,
       })
       .from(PostContentStates)
       .where(eq(PostContentStates.postId, postId))
       .for('update')
       .then(firstOrThrow);
 
-    const updates = await tx
-      .select({ update: PostContentUpdates.update, seq: PostContentUpdates.seq })
-      .from(PostContentUpdates)
-      .where(and(eq(PostContentUpdates.postId, postId), gt(PostContentUpdates.seq, state.seq)))
-      .orderBy(desc(PostContentUpdates.seq));
+    const updates = await redis.smembers(`post:content:updates:${postId}`);
 
     if (updates.length === 0) {
       return false;
     }
 
-    const update = Y.mergeUpdatesV2(updates.map(({ update }) => update));
+    const update = Y.mergeUpdatesV2(updates.map((update) => base64.parse(update)));
     const doc = new Y.Doc();
 
     Y.applyUpdateV2(doc, state.update);
@@ -41,19 +38,14 @@ export const PostContentStateUpdateJob = defineJob('post:content:state-update', 
       .set({
         update: Y.encodeStateAsUpdateV2(doc),
         vector: Y.encodeStateVector(doc),
-        seq: updates[0].seq,
+        updatedAt: dayjs(),
       })
       .where(and(eq(PostContentStates.postId, postId)));
+
+    await redis.srem(`post:content:updates:${postId}`, ...updates);
 
     if (Y.equalSnapshots(prevSnapshot, snapshot)) {
       return;
     }
-
-    await tx
-      .update(PostContentStates)
-      .set({
-        updatedAt: dayjs(),
-      })
-      .where(and(eq(PostContentStates.postId, postId)));
   });
 });
