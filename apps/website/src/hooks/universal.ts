c40ec2f9a89451dbfe3de1efd0e1f818bac55cb0
devunt <337728+devunt@users.noreply.github.com>
2025-03-30 15:57:38 +0000

    website reroute 버그 수정 (#129)

--- apps/website/src/hooks/universal.ts
+++ apps/website/src/hooks/universal.ts
@@ -2,7 +2,7 @@ import { env } from '$env/dynamic/public';
 import type { Reroute } from '@sveltejs/kit';
 
 export const reroute: Reroute = async ({ url }) => {
-  if (url.origin === env.PUBLIC_WEBSITE_URL) {
+  if (url.hostname === env.PUBLIC_WEBSITE_HOST) {
     return `/website${url.pathname}`;
   } else if (url.hostname === env.PUBLIC_USERSITE_HOST) {
     return `/usersite/apex${url.pathname}`;
