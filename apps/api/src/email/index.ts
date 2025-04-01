53dd85128bbd80209a642ec91ad8d53d8f6d92eb
devunt <337728+devunt@users.noreply.github.com>
2025-04-01 08:31:56 +0000

    이메일 발송에 ses 사용 (#156)

--- apps/api/src/email/index.ts
+++ apps/api/src/email/index.ts
@@ -1,4 +1,6 @@
-import { resend } from '@/external/resend';
+import { SendEmailCommand } from '@aws-sdk/client-ses';
+import { render } from '@react-email/components';
+import * as aws from '@/external/aws';
 import type * as React from 'react';
 
 type SendEmailParams = {
@@ -8,10 +10,22 @@ type SendEmailParams = {
 };
 
 export const sendEmail = async ({ subject, recipient, body }: SendEmailParams) => {
-  await resend.emails.send({
-    from: 'Typie <hello@typie.co>',
-    to: [recipient],
-    subject,
-    react: body,
-  });
+  await aws.ses.send(
+    new SendEmailCommand({
+      Source: 'Typie <hello@typie.co>',
+      Destination: {
+        ToAddresses: [recipient],
+      },
+      Message: {
+        Subject: {
+          Data: subject,
+        },
+        Body: {
+          Html: {
+            Data: await render(body),
+          },
+        },
+      },
+    }),
+  );
 };
