8c862334823662d11bc1791b446d0831bd4fb76b
devunt <337728+devunt@users.noreply.github.com>
2025-04-01 12:56:58 +0000

    resend 관련 레코드 제거 (#162)

--- apps/infrastructure/pulumi/aws/route53.ts
+++ apps/infrastructure/pulumi/aws/route53.ts
@@ -41,29 +41,3 @@ new aws.route53.Record('help.typie.co', {
   records: ['cname.rdbl.io'],
   ttl: 300,
 });
-
-new aws.route53.Record('send.typie.co|mx', {
-  zoneId: zones.typie_co.zoneId,
-  type: 'MX',
-  name: 'send.typie.co',
-  records: ['10 feedback-smtp.ap-northeast-1.amazonses.com'],
-  ttl: 300,
-});
-
-new aws.route53.Record('send.typie.co|txt', {
-  zoneId: zones.typie_co.zoneId,
-  type: 'TXT',
-  name: 'send.typie.co',
-  records: ['v=spf1 include:amazonses.com ~all'],
-  ttl: 300,
-});
-
-new aws.route53.Record('resend._domainkey.typie.co|txt', {
-  zoneId: zones.typie_co.zoneId,
-  type: 'TXT',
-  name: 'resend._domainkey.typie.co',
-  records: [
-    'p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC3+gXuQ+MyJT5PUzXZ7UJHY3vfwy41Bh6nmhBHbCDK7WJvr0COTx5LbwYH7+hZLIVLZgevUR4ErJO5w1GPAK29RRZ49iACcUgh4rJBME4l0w1h3vsq1guTkTjR2Uakrjx0r/dNof+XAMSvYJ0GMxF5CY1jFPJ/KmVPfQgROFUF5wIDAQAB',
-  ],
-  ttl: 300,
-});
