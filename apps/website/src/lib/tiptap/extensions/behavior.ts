ab95993ad5280ffd03c44bcf6715dfebf25a6736
devunt <337728+devunt@users.noreply.github.com>
2025-04-02 10:35:45 +0000

    팁탭 tab 키 지원 (#166)

--- apps/website/src/lib/tiptap/extensions/behavior.ts
+++ apps/website/src/lib/tiptap/extensions/behavior.ts
@@ -52,6 +52,54 @@ export const Behavior = Extension.create({
 
       //   return false;
       // },
+
+      Tab: ({ editor }) => {
+        const { selection } = editor.state;
+        const { $anchor } = selection;
+
+        if (editor.isActive('list_item') && $anchor.parentOffset === 0) {
+          const res = editor.chain().sinkListItem('list_item').run();
+          if (res) {
+            return true;
+          }
+        }
+
+        editor
+          .chain()
+          .command(({ tr }) => {
+            tr.insertText('\u0009');
+            return true;
+          })
+          .run();
+
+        return true;
+      },
+
+      'Shift-Tab': ({ editor }) => {
+        const { doc, selection } = editor.state;
+        const { $anchor } = selection;
+
+        if (editor.isActive('list_item') && $anchor.parentOffset === 0) {
+          const res = editor.chain().liftListItem('list_item').run();
+          if (res) {
+            return true;
+          }
+        }
+
+        if (doc.textBetween($anchor.pos - 1, $anchor.pos) === '\u0009') {
+          editor
+            .chain()
+            .command(({ tr }) => {
+              tr.delete($anchor.pos - 1, $anchor.pos);
+              return true;
+            })
+            .run();
+
+          return true;
+        }
+
+        return true;
+      },
     };
   },
 
