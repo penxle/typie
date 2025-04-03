ab1c93d5a50a0bb9b5650fd5d4c85b934bc5a762
devunt <337728+devunt@users.noreply.github.com>
2025-04-03 13:10:56 +0000

    랜딩 스크롤 안 되는 이슈 수정 (#174)

--- apps/website/src/routes/+layout.svelte
+++ apps/website/src/routes/+layout.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
   import '../app.css';
 
-  import { css } from '$styled-system/css';
-
   let { children } = $props();
 </script>
 
-<div class={css({ display: 'flex', flexDirection: 'column', flexGrow: '1', height: 'screen' })}>
-  {@render children()}
-</div>
+{@render children()}
