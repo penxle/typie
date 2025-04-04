5ea90b36cf24cb9af932c475c379ceb75a480056
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2025-04-04 00:52:26 +0000

    dashboard 페이지들 path group으로 묶음 (#175)
    머쓱
    
    - 사이드바 -> dashboard layout으로 이동
    - dashboard layout에서 query.me 체크
    - home, editor -> (dashboard) 안으로 이동

---
+++ apps/website/src/routes/website/(dashboard)/home/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { goto } from '$app/navigation';
+  import { graphql } from '$graphql';
+
+  const query = graphql(`
+    query HomePage_Query {
+      me @required {
+        id
+        email
+      }
+    }
+  `);
+
+  const logout = graphql(`
+    mutation HomePage_Logout_Mutation {
+      logout
+    }
+  `);
+
+  const handleLogout = async () => {
+    await logout();
+    await goto('/');
+  };
+</script>
+
+<div>{$query.me.email}</div>
+<button onclick={handleLogout} type="button">로그아웃</button>
