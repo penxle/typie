2687bf8d4602ef92cc334d130367233b7174daf1
devunt <337728+devunt@users.noreply.github.com>
2025-04-04 07:34:54 +0000

    유저사이트에 TiptapRenderer 적용 (#188)

--- apps/website/src/routes/usersite/wildcard/[slug]/+page.ts
+++ apps/website/src/routes/usersite/wildcard/[slug]/+page.ts
@@ -1,5 +1,6 @@
 import type { UsersiteWildcardSlugPage_Query_Variables } from './$graphql';
 
-export const _UsersiteWildcardSlugPage_Query_Variables: UsersiteWildcardSlugPage_Query_Variables = ({ params }) => ({
+export const _UsersiteWildcardSlugPage_Query_Variables: UsersiteWildcardSlugPage_Query_Variables = ({ params, url }) => ({
+  hostname: url.hostname,
   slug: params.slug,
 });
