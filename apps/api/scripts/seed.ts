8873087f432ff5d5e65145c85955b780c336f101
devunt <337728+devunt@users.noreply.github.com>
2025-04-05 15:48:27 +0000

    db seed 스크립트 추가 (#211)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
      - 초기 플랜 데이터 설정 기능이 추가되어, 새로운 플랜 정보를 쉽게 관리할 수 있습니다.
      - 플랜 식별 및 관리 관련 기능이 개선되었습니다.
    
    - **Refactor**
      - 플랜 설정에서 규칙 입력이 더욱 유연해져, 필수 항목 없이도 일부 정보만 제공할 수 있습니다.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ apps/api/scripts/seed.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env bun
+
+import { PlanId } from '@/const';
+import { db, Plans } from '@/db';
+import { PlanAvailability } from '@/enums';
+
+await db.transaction(async (tx) => {
+  await tx.insert(Plans).values({
+    id: PlanId.PLUS,
+    name: 'Plus',
+    fee: 4900,
+    rules: {},
+  });
+
+  await tx.insert(Plans).values({
+    id: 'PL0PENXLE',
+    name: 'PENXLE',
+    fee: 0,
+    rules: {},
+    availability: PlanAvailability.PRIVATE,
+  });
+});
+
+process.exit(0);
