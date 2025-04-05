f95e198c5e614224873372b7f04eb50b2fe9144e
devunt <337728+devunt@users.noreply.github.com>
2025-04-05 13:10:13 +0000

    에디터가 slug를 이용하도록 함 (#204)
    주소 구조도 같이 바꿈

--- apps/website/src/routes/website/(dashboard)/editor/[postId]/+page.ts
+++
@@ -1,5 +0,0 @@
-import type { EditorPostIdPage_Query_Variables } from './$graphql';
-
-export const _EditorPostIdPage_Query_Variables: EditorPostIdPage_Query_Variables = ({ params }) => ({
-  postId: params.postId,
-});
