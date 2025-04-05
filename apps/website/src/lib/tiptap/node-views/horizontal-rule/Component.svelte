e68c470d6dbb848896fe663781369bfeaf9e5fe4
devunt <337728+devunt@users.noreply.github.com>
2025-04-05 11:10:58 +0000

    기타 노드뷰 디자인 개선 (#202)

--- apps/website/src/lib/tiptap/node-views/horizontal-rule/Component.svelte
+++ apps/website/src/lib/tiptap/node-views/horizontal-rule/Component.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-  import { TiptapNodeViewBubbleMenu } from '$lib/tiptap/components';
   import { NodeView } from '$lib/tiptap/lib';
   import { values } from '$lib/tiptap/values';
-  import { center, flex } from '$styled-system/patterns';
+  import { center } from '$styled-system/patterns';
   import type { NodeViewProps } from '$lib/tiptap/lib';
 
   type Props = NodeViewProps;
 
-  let { editor, node, getPos, updateAttributes }: Props = $props();
+  let { node }: Props = $props();
 
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const { component: Component } = $derived(values.horizontalRule.find(({ type }) => type === node.attrs.type)!);
@@ -16,13 +15,3 @@
 <NodeView style={center.raw({ minHeight: '[1lh]' })}>
   <Component />
 </NodeView>
-
-<TiptapNodeViewBubbleMenu {editor} {getPos} {node}>
-  <div class={flex({ direction: 'column', gap: '4px' })}>
-    {#each values.horizontalRule as { type, component: Component } (type)}
-      <button class={center({ width: '400px', height: '50px' })} onclick={() => updateAttributes({ type })} type="button">
-        <Component />
-      </button>
-    {/each}
-  </div>
-</TiptapNodeViewBubbleMenu>
