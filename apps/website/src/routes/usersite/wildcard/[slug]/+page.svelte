f62d96c8e6f7be037545135faf647ed482621566
devunt <337728+devunt@users.noreply.github.com>
2025-04-06 13:33:41 +0000

    유저사이트 렌더링 개선 (#227)

--- apps/website/src/routes/usersite/wildcard/[slug]/+page.svelte
+++ apps/website/src/routes/usersite/wildcard/[slug]/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-  import { page } from '$app/state';
   import { graphql } from '$graphql';
+  import { HorizontalDivider } from '$lib/components';
   import { TiptapRenderer } from '$lib/tiptap';
   import { css } from '$styled-system/css';
+  import { flex } from '$styled-system/patterns';
 
   const query = graphql(`
     query UsersiteWildcardSlugPage_Query($origin: String!, $slug: String!) {
@@ -18,6 +19,7 @@
             content {
               id
               title
+              subtitle
               body
             }
           }
@@ -32,12 +34,33 @@
   `);
 </script>
 
-usersite wildcard
-<br />
-host: {page.url.hostname}
-<br />
-slug: {page.params.slug}
-<br />
 {#if $query.entityView.node.__typename === 'PostView'}
-  <TiptapRenderer style={css.raw({ borderWidth: '1px', padding: '16px' })} content={$query.entityView.node.content.body} />
+  <div class={flex({ flexDirection: 'column', alignItems: 'center', width: 'full', backgroundColor: 'gray.100' })}>
+    <div
+      class={flex({
+        flexDirection: 'column',
+        alignItems: 'center',
+        paddingY: '80px',
+        width: 'full',
+        maxWidth: '1200px',
+        backgroundColor: 'white',
+      })}
+    >
+      <div class={flex({ flexDirection: 'column', width: 'full', maxWidth: '1000px' })}>
+        <div class={css({ fontSize: '28px', fontWeight: 'bold' })}>
+          {$query.entityView.node.content.title}
+        </div>
+
+        {#if $query.entityView.node.content.subtitle}
+          <div class={css({ marginTop: '4px', fontSize: '16px', fontWeight: 'medium' })}>
+            {$query.entityView.node.content.subtitle}
+          </div>
+        {/if}
+
+        <HorizontalDivider style={css.raw({ marginTop: '10px', marginBottom: '20px' })} />
+      </div>
+
+      <TiptapRenderer style={css.raw({ width: 'full' })} content={$query.entityView.node.content.body} />
+    </div>
+  </div>
 {/if}
