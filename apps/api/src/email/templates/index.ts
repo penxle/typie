18964cdadf90230806f420015a65aea28063d812
devunt <337728+devunt@users.noreply.github.com>
2025-04-06 08:59:39 +0000

    사전등록 완료시 자동 이메일 발송 (#219)

--- apps/api/src/email/templates/index.ts
+++ apps/api/src/email/templates/index.ts
@@ -1,4 +1,5 @@
 export { default as EmailUpdatedEmail } from './EmailUpdatedEmail';
 export { default as EmailUpdateEmail } from './EmailUpdateEmail';
 export { default as PasswordResetEmail } from './PasswordResetEmail';
+export { default as PreorderCompletedEmail } from './PreorderCompletedEmail';
 export { default as SignUpEmail } from './SignUpEmail';
