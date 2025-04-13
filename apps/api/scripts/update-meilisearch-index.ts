570299248e54e18fae618d373b373e2c585f15f1
devunt <337728+devunt@users.noreply.github.com>
2025-04-13 07:37:31 +0000

    meilisearch 인덱스 생성 및 업데이트 스크립트 추가 (#351)

---
+++ apps/api/scripts/update-meilisearch-index.ts
@@ -0,0 +1,35 @@
+#!/usr/bin/env bun
+
+import { eq } from 'drizzle-orm';
+import { db, Entities, PostContents, Posts } from '@/db';
+import { EntityState } from '@/enums';
+import { meili } from '@/search';
+
+const index = meili.index('posts');
+
+const posts = await db
+  .select({
+    id: Posts.id,
+    siteId: Entities.siteId,
+    title: Posts.title,
+    subtitle: Posts.subtitle,
+    text: PostContents.text,
+    updatedAt: Posts.updatedAt,
+  })
+  .from(Posts)
+  .innerJoin(Entities, eq(Entities.id, Posts.entityId))
+  .innerJoin(PostContents, eq(PostContents.postId, Posts.id))
+  .where(eq(Entities.state, EntityState.ACTIVE));
+
+const deletedPosts = await db
+  .select({ id: Posts.id })
+  .from(Posts)
+  .innerJoin(Entities, eq(Entities.id, Posts.entityId))
+  .where(eq(Entities.state, EntityState.DELETED));
+
+await index.deleteDocuments(deletedPosts.map(({ id }) => id));
+await index.addDocuments(posts.map(({ updatedAt, ...rest }) => ({ ...rest, updatedAt: updatedAt.unix() })));
+
+console.log('Indexed posts:', posts.length);
+
+process.exit(0);
