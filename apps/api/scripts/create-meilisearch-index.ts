e89b9715d3fdbaad5c3c8eccf4a6824b9ba6b81d
devunt <337728+devunt@users.noreply.github.com>
2025-04-13 07:37:31 +0000

    meilisearch 인덱스 생성 및 업데이트 스크립트 추가 (#351)

--- apps/api/scripts/create-meilisearch-index.ts
+++ apps/api/scripts/create-meilisearch-index.ts
@@ -2,12 +2,19 @@
 
 import { meili } from '@/search';
 
+await meili.deleteIndexIfExists('posts');
 await meili.createIndex('posts', {
   primaryKey: 'id',
 });
 
 const posts = meili.index('posts');
+await posts.updateSettings({
+  searchableAttributes: ['title', 'subtitle', 'text'],
+  filterableAttributes: ['siteId', 'updatedAt'],
+  sortableAttributes: ['updatedAt'],
+  typoTolerance: { enabled: false },
+});
 
-await posts.updateSearchableAttributes(['title', 'subtitle', 'text']);
-await posts.updateFilterableAttributes(['siteId', 'updatedAt']);
-await posts.updateSortableAttributes(['updatedAt']);
+console.log('Meilisearch index recreated.');
+
+process.exit(0);
