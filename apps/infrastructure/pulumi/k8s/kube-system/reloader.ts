b47636774bba37327db75ae27cfd3ec5ddad5d85
devunt <337728+devunt@users.noreply.github.com>
2025-04-20 07:20:23 +0000

    k8s reloader 버전 다운그레이드 (#424)

--- apps/infrastructure/pulumi/k8s/kube-system/reloader.ts
+++ apps/infrastructure/pulumi/k8s/kube-system/reloader.ts
@@ -2,6 +2,7 @@ import * as k8s from '@pulumi/kubernetes';
 
 new k8s.helm.v4.Chart('reloader', {
   chart: 'reloader',
+  version: '2.0.0',
   namespace: 'kube-system',
   repositoryOpts: {
     repo: 'https://stakater.github.io/stakater-charts',
