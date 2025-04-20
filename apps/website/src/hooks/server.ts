8f5d8585c31562a4725eebbde8ea090aaf9af33b
devunt <337728+devunt@users.noreply.github.com>
2025-04-20 13:20:33 +0000

    클라이언트 IP 추출 로직 개선 (#433)

--- apps/website/src/hooks/server.ts
+++ apps/website/src/hooks/server.ts
@@ -2,21 +2,9 @@ import './common';
 
 import { sequence } from '@sveltejs/kit/hooks';
 import { logger, logging } from '@typie/lib/svelte';
-import { env } from '$env/dynamic/public';
-import type { HandleFetch, HandleServerError } from '@sveltejs/kit';
+import type { HandleServerError } from '@sveltejs/kit';
 
 export const handle = sequence(logging);
-
-export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
-  const url = new URL(request.url);
-
-  if (url.origin === env.PUBLIC_API_URL) {
-    request.headers.set('x-sveltekit-ip', event.getClientAddress());
-    return await fetch(request);
-  }
-
-  return await fetch(request);
-};
 
 export const handleError: HandleServerError = ({ event, error, status, message }) => {
   logger.error({
