3d0b7601e086653a713e1bdc964acc6a591dbf13
devunt <337728+devunt@users.noreply.github.com>
2025-04-20 06:07:05 +0000

    auth 서버 인덱스 접속시 authorize로 리다이렉트 (#420)

--- apps/website/src/routes/usersite/apex/(index)/+server.ts
+++ apps/website/src/routes/usersite/apex/(index)/+server.ts
@@ -1,6 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 import { env } from '$env/dynamic/public';
+import type { RequestHandler } from './$types';
 
-export const GET = async () => {
+export const GET: RequestHandler = async () => {
   redirect(301, env.PUBLIC_WEBSITE_URL);
 };
