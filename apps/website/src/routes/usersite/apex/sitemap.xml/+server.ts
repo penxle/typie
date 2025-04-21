9cb3585c185901417e046f2eb2904b7314ea80d9
devunt <337728+devunt@users.noreply.github.com>
2025-04-21 19:12:25 +0000

    전역 robots.txt 핸들링 및 사이트별 sitemap.xml 추가 (#447)

---
+++ apps/website/src/routes/usersite/apex/sitemap.xml/+server.ts
@@ -0,0 +1,6 @@
+import { sitemap } from '@typie/lib/svelte';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async (event) => {
+  return sitemap(event, ['/']);
+};
