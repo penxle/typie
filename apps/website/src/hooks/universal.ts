7dd3c1675d91b9ceea0f1dcea93ae62b56061f83
devunt <337728+devunt@users.noreply.github.com>
2025-04-21 19:20:42 +0000

    robots.txt 라우팅 버그 수정 (#450)

--- apps/website/src/hooks/universal.ts
+++ apps/website/src/hooks/universal.ts
@@ -2,7 +2,7 @@ import { env } from '$env/dynamic/public';
 import type { Reroute } from '@sveltejs/kit';
 
 export const reroute: Reroute = async ({ url }) => {
-  if (url.pathname === '/graphql') {
+  if (url.pathname === '/graphql' || url.pathname === '/robots.txt') {
     return url.pathname;
   } else if (url.origin === env.PUBLIC_AUTH_URL) {
     return `/auth${url.pathname}`;
