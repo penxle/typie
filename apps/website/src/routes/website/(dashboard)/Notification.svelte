10885de6d8d987303d090fba63a2bbe158247f21
devunt <337728+devunt@users.noreply.github.com>
2025-04-27 07:33:54 +0000

    사이드바 drawer 컴포넌트 마운트가 아닌 css transition으로 구현 (#518)
    mount/unmount가 아닌 css transition으로 동일 효과 구현해 닫았다 열어도 상태 유지되도록 함

--- apps/website/src/routes/website/(dashboard)/Notification.svelte
+++ apps/website/src/routes/website/(dashboard)/Notification.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import dayjs from 'dayjs';
-  import { fly } from 'svelte/transition';
   import BellIcon from '~icons/lucide/bell';
+  import CheckCheckIcon from '~icons/lucide/check-check';
   import { fragment, graphql } from '$graphql';
+  import { portal, tooltip } from '$lib/actions';
+  import { Icon } from '$lib/components';
   import { css } from '$styled-system/css';
-  import { flex } from '$styled-system/patterns';
+  import { center, flex } from '$styled-system/patterns';
   import SidebarButton from './SidebarButton.svelte';
   import type { DashboardLayout_Notification_user } from '$graphql';
 
@@ -83,146 +85,160 @@
 <SidebarButton active={open} icon={BellIcon} label="알림" onclick={() => (open = true)} />
 
 {#if open}
-  <div class={css({ position: 'fixed', inset: '0', zIndex: '50' })}>
-    <div class={css({ position: 'absolute', inset: '0' })} onclick={() => (open = false)} role="none"></div>
+  <div class={css({ position: 'fixed', inset: '0' })} onclick={() => (open = false)} role="none" use:portal></div>
+{/if}
+
+<div
+  class={css({
+    position: 'fixed',
+    left: open ? '64px' : '59px',
+    insetY: '0',
+    width: '0',
+    backgroundColor: 'white',
+    boxShadow: 'small',
+    opacity: open ? '100' : '0',
+    zIndex: '50',
+    transitionProperty: 'left, opacity',
+    transitionDuration: '100ms',
+    transitionTimingFunction: 'cubic-bezier(0.33, 1, 0.68, 1)',
+    overflowX: 'hidden',
+  })}
+  ontransitionend={(e) => {
+    if (!open) {
+      e.currentTarget.style.width = '0';
+    }
+  }}
+  ontransitionstart={(e) => {
+    if (open) {
+      e.currentTarget.style.width = '350px';
+    }
+  }}
+  use:portal
+>
+  <div
+    class={flex({
+      flexDirection: 'column',
+      borderRightWidth: '1px',
+      borderRightColor: 'gray.100',
+      borderRightRadius: '4px',
+      size: 'full',
+    })}
+  >
     <div
       class={flex({
-        position: 'absolute',
-        left: '64px',
-        insetY: '0',
-        flexDirection: 'column',
-        borderRightWidth: '1px',
-        borderColor: 'gray.100',
-        borderRightRadius: '4px',
-        paddingX: '6px',
-        width: '350px',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexShrink: '0',
+        gap: '4px',
+        paddingX: '12px',
+        paddingY: '8px',
         backgroundColor: 'white',
-        boxShadow: 'small',
-        overflowY: 'auto',
-        zIndex: '50',
       })}
-      transition:fly={{ x: -5, duration: 100 }}
     >
-      <div
-        class={css({
-          position: 'sticky',
-          top: '0',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          flexShrink: '0',
-          paddingX: '12px',
-          backgroundColor: 'white',
-          height: '46px',
-          zIndex: '1',
-        })}
-      >
-        <p class={css({ fontSize: '14px', fontWeight: 'bold' })}>알림</p>
+      <p class={css({ fontSize: '14px', fontWeight: 'bold' })}>알림</p>
 
-        <button
+      <button
+        class={center({
+          borderRadius: '4px',
+          size: '24px',
+          color: 'gray.500',
+          transition: 'common',
+          _hover: { color: 'gray.700', backgroundColor: 'gray.100' },
+        })}
+        onclick={async () => {
+          await markAllNotificationsAsRead();
+        }}
+        type="button"
+        use:tooltip={{ message: '모두 읽기' }}
+      >
+        <Icon icon={CheckCheckIcon} />
+      </button>
+    </div>
+
+    <ul class={flex({ direction: 'column', gap: '4px', flexGrow: '1', marginTop: '4px', paddingBottom: '6px' })}>
+      {#each $user.notifications as notification (notification.id)}
+        <li
           class={css({
-            borderRadius: '4px',
+            borderRadius: '6px',
             fontSize: '14px',
             fontWeight: 'medium',
-            color: 'brand.500',
-            paddingX: '6px',
-            paddingY: '2px',
+            color: 'gray.700',
             _hover: { backgroundColor: 'gray.100' },
           })}
-          onclick={async () => {
-            await markAllNotificationsAsRead();
-          }}
-          type="button"
         >
-          모두 읽기
-        </button>
-      </div>
-
-      <ul class={flex({ direction: 'column', gap: '4px', flexGrow: '1', marginTop: '4px', paddingBottom: '6px' })}>
-        {#each $user.notifications as notification (notification.id)}
-          <li
-            class={css({
-              borderRadius: '6px',
-              fontSize: '14px',
-              fontWeight: 'medium',
-              color: 'gray.700',
-              _hover: { backgroundColor: 'gray.100' },
+          <button
+            class={flex({
+              justify: 'space-between',
+              gap: '8px',
+              position: 'relative',
+              paddingX: '12px',
+              paddingY: '10px',
+              width: 'full',
             })}
+            onclick={async () => {
+              if (notification.state === 'UNREAD') {
+                await markNotificationAsRead({ notificationId: notification.id });
+              }
+
+              const url =
+                notification.data.__typename === 'NotificationCommentData'
+                  ? notification.data.post.view.entity.url
+                  : notification.data.link;
+
+              if (url) {
+                window.open(url, '_blank');
+              }
+            }}
+            type="button"
           >
-            <button
-              class={flex({
-                justify: 'space-between',
-                gap: '8px',
-                position: 'relative',
-                paddingX: '12px',
-                paddingY: '10px',
-                width: 'full',
-              })}
-              onclick={async () => {
-                if (notification.state === 'UNREAD') {
-                  await markNotificationAsRead({ notificationId: notification.id });
-                }
+            {#if notification.state === 'UNREAD'}
+              <div
+                class={css({
+                  position: 'absolute',
+                  top: '10px',
+                  left: '6px',
+                  borderRadius: 'full',
+                  size: '4px',
+                  backgroundColor: 'brand.500',
+                })}
+              ></div>
+            {/if}
 
-                const url =
-                  notification.data.__typename === 'NotificationCommentData'
-                    ? notification.data.post.view.entity.url
-                    : notification.data.link;
-
-                if (url) {
-                  window.open(url, '_blank');
-                }
-              }}
-              type="button"
-            >
-              {#if notification.state === 'UNREAD'}
-                <div
-                  class={css({
-                    position: 'absolute',
-                    top: '10px',
-                    left: '6px',
-                    borderRadius: 'full',
-                    size: '4px',
-                    backgroundColor: 'brand.500',
-                  })}
-                ></div>
-              {/if}
-
-              {#if notification.data.__typename === 'NotificationAnnouncementData'}
-                <p>{notification.data.message}</p>
-              {:else if notification.data.__typename === 'NotificationCommentData'}
-                <p class={css({ textAlign: 'left', width: 'full' })}>
-                  <strong
-                    class={css({ display: 'inline-block', fontWeight: 'bold', verticalAlign: 'bottom', maxWidth: '200px', truncate: true })}
-                  >
-                    {notification.data.post.title}
-                  </strong>
-                  에 댓글이 달렸어요
-                </p>
-
-                <date
-                  class={css({ display: 'block', flex: 'none', marginTop: '2px', fontSize: '12px', color: 'gray.400' })}
-                  datetime={notification.data.comment.createdAt}
+            {#if notification.data.__typename === 'NotificationAnnouncementData'}
+              <p>{notification.data.message}</p>
+            {:else if notification.data.__typename === 'NotificationCommentData'}
+              <p class={css({ textAlign: 'left', width: 'full' })}>
+                <strong
+                  class={css({ display: 'inline-block', fontWeight: 'bold', verticalAlign: 'bottom', maxWidth: '200px', truncate: true })}
                 >
-                  {dayjs(notification.data.comment.createdAt).formatAsDateTime()}
-                </date>
-              {/if}
-            </button>
-          </li>
-        {:else}
-          <li
-            class={css({
-              marginY: 'auto',
-              paddingBottom: '32px',
-              fontSize: '14px',
-              textAlign: 'center',
-              fontWeight: 'medium',
-              color: 'gray.400',
-            })}
-          >
-            알림이 없어요
-          </li>
-        {/each}
-      </ul>
-    </div>
+                  {notification.data.post.title}
+                </strong>
+                에 댓글이 달렸어요
+              </p>
+
+              <date
+                class={css({ display: 'block', flex: 'none', marginTop: '2px', fontSize: '12px', color: 'gray.400' })}
+                datetime={notification.data.comment.createdAt}
+              >
+                {dayjs(notification.data.comment.createdAt).formatAsDateTime()}
+              </date>
+            {/if}
+          </button>
+        </li>
+      {:else}
+        <li
+          class={css({
+            marginY: 'auto',
+            paddingBottom: '32px',
+            fontSize: '14px',
+            textAlign: 'center',
+            fontWeight: 'medium',
+            color: 'gray.400',
+          })}
+        >
+          알림이 없어요
+        </li>
+      {/each}
+    </ul>
   </div>
-{/if}
+</div>
