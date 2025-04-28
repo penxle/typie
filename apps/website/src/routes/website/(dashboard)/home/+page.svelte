6c2705466dd9de47cb198ccb6ef59b1e7cbe30a8
devunt <337728+devunt@users.noreply.github.com>
2025-04-28 09:08:34 +0000

    홈에서 포스트 없을 경우 핸들링함 (#545)

--- apps/website/src/routes/website/(dashboard)/home/+page.svelte
+++ apps/website/src/routes/website/(dashboard)/home/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import FilePenIcon from '~icons/lucide/file-pen';
+  import { goto } from '$app/navigation';
   import { graphql } from '$graphql';
-  import { Helmet, Icon } from '$lib/components';
+  import { Button, Helmet, Icon } from '$lib/components';
   import { css } from '$styled-system/css';
-  import { center } from '$styled-system/patterns';
+  import { center, flex } from '$styled-system/patterns';
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const query = graphql(`
     query HomePage_Query {
       me @required {
@@ -22,14 +22,37 @@
       }
     }
   `);
+
+  const createPost = graphql(`
+    mutation HomePage_CreatePost_Mutation($input: CreatePostInput!) {
+      createPost(input: $input) {
+        id
+
+        entity {
+          id
+          slug
+        }
+      }
+    }
+  `);
 </script>
 
 <Helmet title="홈" />
 
-<div class={center({ flexDirection: 'column', size: 'full' })}>
-  <Icon style={css.raw({ size: '60px' })} icon={FilePenIcon} />
-  <div class={css({ textAlign: 'center' })}>
-    <h1 class={css({ fontSize: '22px', fontWeight: 'bold' })}>첫 페이지를 만들어보세요</h1>
-    <p class={css({ fontSize: '15px', color: 'gray.500' })}>아직 페이지가 없어요.</p>
+<div class={center({ flexDirection: 'column', gap: '20px', size: 'full' })}>
+  <Icon style={css.raw({ size: '56px', color: 'gray.700', '& *': { strokeWidth: '[1.25px]' } })} icon={FilePenIcon} />
+
+  <div class={flex({ flexDirection: 'column', alignItems: 'center', gap: '4px' })}>
+    <h1 class={css({ fontSize: '16px', fontWeight: 'bold', color: 'gray.700' })}>첫 포스트를 만들어보세요</h1>
+    <p class={css({ fontSize: '14px', color: 'gray.500' })}>아래 버튼을 눌러 포스트를 만들 수 있어요</p>
   </div>
+
+  <Button
+    onclick={async () => {
+      const resp = await createPost({ siteId: $query.me.sites[0].id });
+      await goto(`/${resp.entity.slug}`);
+    }}
+  >
+    새 포스트 만들기
+  </Button>
 </div>
