0f8238ac7dd599d4267beb234ae8c9f34aa7ff2a
robin-maki <8201350+robin-maki@users.noreply.github.com>
2025-04-28 07:43:06 +0000

    본인 포스트는 비공개여도 조회 가능하게 수정 (#538)

--- apps/api/src/utils/permission.ts
+++ apps/api/src/utils/permission.ts
@@ -3,10 +3,14 @@ import { db, firstOrThrow, Sites } from '@/db';
 import { TypieError } from '@/errors';
 
 type AssertSitePermissionParams = {
-  userId: string;
+  userId: string | undefined;
   siteId: string;
 };
 export const assertSitePermission = async ({ userId, siteId }: AssertSitePermissionParams) => {
+  if (!userId) {
+    throw new TypieError({ code: 'permission_denied' });
+  }
+
   const site = await db.select({ userId: Sites.userId }).from(Sites).where(eq(Sites.id, siteId)).then(firstOrThrow);
 
   if (site.userId !== userId) {
