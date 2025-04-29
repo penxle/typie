663b8ff956a5ec6ac8ab155a4b99d91b732ae90f
devunt <337728+devunt@users.noreply.github.com>
2025-04-29 15:15:10 +0000

    글자 수 제한 extension 구현 (#571)

--- apps/website/src/lib/tiptap/extensions/index.ts
+++ apps/website/src/lib/tiptap/extensions/index.ts
@@ -3,6 +3,7 @@ export * from './block-selection';
 export * from './collaboration';
 export * from './command';
 export * from './drop-cursor';
+export * from './limit';
 export * from './placeholder';
 export * from './syntax-highlight';
 export * from './typography';
