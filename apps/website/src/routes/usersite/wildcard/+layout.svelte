9eea59fe72eb3cebfa08ff64f755a8f259717570
devunt <337728+devunt@users.noreply.github.com>
2025-04-30 11:38:37 +0000

    mixpanel identity 추가 (#599)

--- apps/website/src/routes/usersite/wildcard/+layout.svelte
+++ apps/website/src/routes/usersite/wildcard/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import mixpanel from 'mixpanel-browser';
   import qs from 'query-string';
   import { onMount } from 'svelte';
   import { page } from '$app/state';
@@ -17,6 +18,13 @@
     query UsersiteWildcard_Layout_Query {
       me {
         id
+        name
+        email
+
+        avatar {
+          id
+          url
+        }
       }
     }
   `);
@@ -32,6 +40,18 @@
           state: serializeOAuthState({ redirect_uri: page.url.href }),
           prompt: 'none',
         },
+      });
+    }
+  });
+
+  $effect(() => {
+    if ($query.me) {
+      mixpanel.identify($query.me.id);
+
+      mixpanel.people.set({
+        $email: $query.me.email,
+        $name: $query.me.name,
+        $avatar: qs.stringifyUrl({ url: $query.me.avatar.url, query: { s: 256, f: 'png' } }),
       });
     }
   });
--- apps/website/src/routes/website/(dashboard)/+layout.svelte
+++ apps/website/src/routes/website/(dashboard)/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import mixpanel from 'mixpanel-browser';
+  import qs from 'query-string';
   import { untrack } from 'svelte';
   import Logo from '$assets/logos/logo.svg?component';
   import { graphql } from '$graphql';
@@ -18,7 +20,13 @@
     query DashboardLayout_Query {
       me @required {
         id
+        name
         email
+
+        avatar {
+          id
+          url
+        }
 
         sites {
           id
@@ -94,6 +102,18 @@
         unsubscribe2();
       };
     });
+  });
+
+  $effect(() => {
+    if ($query.me) {
+      mixpanel.identify($query.me.id);
+
+      mixpanel.people.set({
+        $email: $query.me.email,
+        $name: $query.me.name,
+        $avatar: qs.stringifyUrl({ url: $query.me.avatar.url, query: { s: 256, f: 'png' } }),
+      });
+    }
   });
 </script>
 
