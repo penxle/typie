c743cfda4d93140f4195fffc090a1dcdd134f360
devunt <337728+devunt@users.noreply.github.com>
2025-05-04 05:35:15 +0000

    usersite apex에서 permalink 리다이렉트시 인증서버 거쳐가도록 함 (#630)
    TYP-36 해결함

--- apps/website/src/routes/usersite/apex/[permalink]/+page.svelte
+++ apps/website/src/routes/usersite/apex/[permalink]/+page.svelte
@@ -4,14 +4,9 @@
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const query = graphql(`
     query UsersiteApexPermalinkPage_Query($permalink: String!) {
-      entityViewByPermalink(permalink: $permalink) {
-        id
-        slug
-
-        site {
-          id
-          url
-        }
+      permalink(permalink: $permalink) {
+        siteUrl
+        entitySlug
       }
     }
   `);
--- apps/website/src/routes/usersite/apex/[permalink]/+page.ts
+++ apps/website/src/routes/usersite/apex/[permalink]/+page.ts
@@ -1,4 +1,7 @@
 import { redirect } from '@sveltejs/kit';
+import qs from 'query-string';
+import { env } from '$env/dynamic/public';
+import { serializeOAuthState } from '$lib/utils/auth';
 import type { UsersiteApexPermalinkPage_Query_AfterLoad, UsersiteApexPermalinkPage_Query_Variables } from './$graphql';
 
 export const _UsersiteApexPermalinkPage_Query_Variables: UsersiteApexPermalinkPage_Query_Variables = ({ params }) => ({
@@ -6,5 +9,15 @@ export const _UsersiteApexPermalinkPage_Query_Variables: UsersiteApexPermalinkPa
 });
 
 export const _UsersiteApexPermalinkPage_Query_AfterLoad: UsersiteApexPermalinkPage_Query_AfterLoad = ({ query }) => {
-  redirect(302, `${query.entityViewByPermalink.site.url}/${query.entityViewByPermalink.slug}`);
+  const authorizeUrl = qs.stringifyUrl({
+    url: `${env.PUBLIC_AUTH_URL}/authorize`,
+    query: {
+      client_id: env.PUBLIC_OIDC_CLIENT_ID,
+      response_type: 'code',
+      redirect_uri: `${query.permalink.siteUrl}/authorize`,
+      state: serializeOAuthState({ redirect_uri: `${query.permalink.siteUrl}/${query.permalink.entitySlug}` }),
+    },
+  });
+
+  redirect(302, authorizeUrl);
 };
