7a980474c21be90d0cf4c88a0c51e76039948555
devunt <337728+devunt@users.noreply.github.com>
2025-05-11 14:53:51 +0000

    앱 토스트 직접 구현 (#655)

--- apps/mobile/lib/context/toast.dart
+++ apps/mobile/lib/context/toast.dart
@@ -1,112 +1,122 @@
+import 'dart:async';
+
 import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
-import 'package:flutter/widgets.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:typie/icons/lucide.dart';
+import 'package:typie/icons/typie.dart';
 import 'package:typie/styles/colors.dart';
-import 'package:typie/widgets/tappable.dart';
-
-class _TextToastWidget extends StatelessWidget {
-  const _TextToastWidget({required this.type, required this.message, this.actionText, this.onAction});
-
-  final ToastType type;
-  final String message;
-  final String? actionText;
-  final void Function()? onAction;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const Pad(all: 14),
-      decoration: BoxDecoration(color: AppColors.gray_600, borderRadius: BorderRadius.circular(4)),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const Pad(top: 2),
-            child: switch (type) {
-              ToastType.success => const Icon(LucideIcons.circle_check, color: AppColors.green_500, size: 16),
-              ToastType.error => const Icon(LucideIcons.circle_alert, color: AppColors.red_500, size: 16),
-            },
-          ),
-          const Box.gap(6),
-          Expanded(
-            child: Text(
-              message,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: AppColors.white),
-            ),
-          ),
-          if (actionText != null) ...[
-            const Box.gap(16),
-            Tappable(
-              onTap: onAction,
-              child: Text(
-                actionText!,
-                style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w700, color: AppColors.white),
-              ),
-            ),
-          ],
-        ],
-      ),
-    );
-  }
-}
-
-class ToastProvider extends StatelessWidget {
-  const ToastProvider({required this.child, super.key});
-
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return Overlay(
-      initialEntries: <OverlayEntry>[
-        OverlayEntry(
-          builder: (context) {
-            return ToastScope(toast: FToast()..init(context), child: child);
-          },
-        ),
-      ],
-    );
-  }
-}
-
-class ToastScope extends InheritedWidget {
-  const ToastScope({required this.toast, required super.child, super.key});
-
-  final FToast toast;
-
-  static FToast of(BuildContext context) {
-    final scope = context.getInheritedWidgetOfExactType<ToastScope>();
-    return scope!.toast;
-  }
-
-  @override
-  bool updateShouldNotify(ToastScope old) => false;
-}
 
 enum ToastType { success, error }
 
-extension ToastExtension on BuildContext {
-  FToast get _toast => ToastScope.of(this);
+class _Widget extends HookWidget {
+  const _Widget({required this.type, required this.message, required this.completer});
 
-  void toast(ToastType type, String message, {String? actionText, void Function()? onAction}) {
-    _toast.showToast(
-      child: _TextToastWidget(
-        type: type,
-        message: message,
-        actionText: actionText,
-        onAction: () {
-          _toast.removeCustomToast();
-          onAction?.call();
-        },
+  final Completer<void> completer;
+
+  final ToastType type;
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    final animationController = useAnimationController(duration: const Duration(milliseconds: 200));
+    final tweenedOpacity = useMemoized(() {
+      final curve = CurvedAnimation(parent: animationController, curve: Curves.easeOut, reverseCurve: Curves.easeIn);
+      return Tween<double>(begin: 0, end: 1).animate(curve);
+    }, [animationController]);
+    final tweenedOffset = useMemoized(() {
+      final curve = CurvedAnimation(parent: animationController, curve: Curves.easeOut, reverseCurve: Curves.easeIn);
+      return Tween<Offset>(begin: const Offset(0, 0.2), end: Offset.zero).animate(curve);
+    }, [animationController]);
+
+    useEffect(() {
+      animationController.forward();
+
+      final timer = Timer(const Duration(seconds: 2), () {
+        animationController.reverse().then((_) {
+          completer.complete();
+        });
+      });
+
+      return timer.cancel;
+    }, [animationController]);
+
+    final mediaQuery = MediaQuery.of(context);
+    final safeAreaBottom = mediaQuery.padding.bottom;
+    final keyboardHeight = mediaQuery.viewInsets.bottom;
+
+    return Positioned(
+      bottom: safeAreaBottom + keyboardHeight + 12,
+      left: 24,
+      right: 24,
+      child: Material(
+        type: MaterialType.transparency,
+        child: SlideTransition(
+          position: tweenedOffset,
+          child: FadeTransition(
+            opacity: tweenedOpacity,
+            child: Box(
+              decoration: BoxDecoration(color: AppColors.gray_950, borderRadius: BorderRadius.circular(999)),
+              padding: const Pad(all: 12),
+              child: Row(
+                children: [
+                  Box(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(999),
+                      color: switch (type) {
+                        ToastType.success => AppColors.green_600,
+                        ToastType.error => AppColors.red_600,
+                      },
+                    ),
+                    width: 20,
+                    height: 20,
+                    child: Center(
+                      child: switch (type) {
+                        ToastType.success => const Icon(LucideIcons.check, color: AppColors.white, size: 14),
+                        ToastType.error => const Icon(TypieIcons.exclamation, color: AppColors.white, size: 14),
+                      },
+                    ),
+                  ),
+                  const Box.gap(8),
+                  Expanded(
+                    child: Text(
+                      message,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: AppColors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
-      positionedToastBuilder: (context, child, gravity) {
-        final height = MediaQuery.of(context).padding.bottom;
-        final inset = MediaQuery.of(context).viewInsets.bottom;
-        return Positioned(bottom: height + inset + 12, left: 20, right: 20, child: child);
+    );
+  }
+}
+
+extension ToastExtension on BuildContext {
+  static OverlayEntry? _entry;
+
+  void toast(ToastType type, String message) {
+    if (_entry != null) {
+      _entry?.remove();
+    }
+
+    final completer = Completer<void>();
+
+    _entry = OverlayEntry(
+      builder: (context) {
+        return _Widget(type: type, message: message, completer: completer);
       },
     );
+
+    Overlay.of(this, rootOverlay: true).insert(_entry!);
+
+    completer.future.then((_) {
+      _entry?.remove();
+      _entry = null;
+    });
   }
 }
