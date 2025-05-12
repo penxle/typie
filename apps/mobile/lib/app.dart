1d77fd25818a68ad6de69400cb12eee93816d1fa
devunt <337728+devunt@users.noreply.github.com>
2025-05-12 14:00:17 +0000

    provider 위젯들 offstage 처리 (#661)

--- apps/mobile/lib/app.dart
+++ apps/mobile/lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:typie/firebase/push_notification.dart';
+import 'package:typie/providers/push_notification.dart';
 import 'package:typie/routers/app.dart';
 import 'package:typie/styles/colors.dart';
 
@@ -44,7 +44,12 @@ class App extends HookWidget {
           initialEntries: [
             OverlayEntry(
               builder: (context) {
-                return PushNotification(child: KeyboardDismiss(child: child!));
+                return Stack(
+                  children: [
+                    KeyboardDismiss(child: child!),
+                    const Offstage(child: Stack(children: [PushNotificationProvider()])),
+                  ],
+                );
               },
             ),
           ],
