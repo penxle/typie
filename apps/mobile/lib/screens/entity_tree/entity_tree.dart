783fdfb1df40c5f4a9b9e2f72141c698ca397a94
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2025-05-15 02:10:18 +0000

    엔티티 순서 변경 구현 (#672)

--- apps/mobile/lib/screens/entity_tree/entity_tree.dart
+++ apps/mobile/lib/screens/entity_tree/entity_tree.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:typie/graphql/client.dart';
+import 'package:typie/hooks/service.dart';
 import 'package:typie/screens/entity_tree/__generated__/entity_tree_entity_entity.data.gql.dart';
+import 'package:typie/screens/entity_tree/__generated__/entity_tree_move_entity_mutation.req.gql.dart';
 import 'package:typie/screens/entity_tree/entity.dart';
 
 class EntityTree extends HookWidget {
@@ -10,14 +13,48 @@ class EntityTree extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final client = useService<GraphQLClient>();
+
     if (entities.isEmpty) {
       return const Center(child: Text('폴더가 비어있어요', style: TextStyle(fontSize: 16, color: Colors.grey)));
     }
 
-    return ListView.builder(
+    return ReorderableListView.builder(
       itemCount: entities.length,
       itemBuilder: (context, index) {
-        return ListTile(title: Entity(entities[index]));
+        return ListTile(key: ValueKey(entities[index].id), title: Entity(entities[index]));
+      },
+      onReorder: (oldIndex, newIndex) async {
+        String? lowerOrder;
+        String? upperOrder;
+
+        if (newIndex >= entities.length) {
+          lowerOrder = entities[entities.length - 1].order;
+        } else if (newIndex == 0) {
+          upperOrder = entities[0].order;
+        } else {
+          lowerOrder = entities[newIndex - 1].order;
+          upperOrder = entities[newIndex].order;
+        }
+
+        await client.request(
+          GEntityTree_MoveEntity_MutationReq(
+            (b) =>
+                b
+                  ..vars.input.entityId = entities[oldIndex].id
+                  ..vars.input.lowerOrder = lowerOrder
+                  ..vars.input.upperOrder = upperOrder,
+          ),
+        );
+      },
+      proxyDecorator: (child, index, animation) {
+        return Material(
+          color: Colors.transparent,
+          child: Container(
+            decoration: BoxDecoration(color: Colors.grey.shade200, borderRadius: BorderRadius.circular(8)),
+            child: child,
+          ),
+        );
       },
     );
   }
