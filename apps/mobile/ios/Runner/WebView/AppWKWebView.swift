ccbae310f84f78d02090d86d49467446951295df
devunt <337728+devunt@users.noreply.github.com>
2025-05-16 13:59:57 +0000

    iOS 메서드 swizzling 추가 (#680)

--- apps/mobile/ios/Runner/WebView/AppWKWebView.swift
+++ apps/mobile/ios/Runner/WebView/AppWKWebView.swift
@@ -1,5 +1,7 @@
 import WebKit
 
+typealias ClosureType = @convention(c) (Any, Selector, UnsafeRawPointer, Bool, Bool, Bool, Any?) -> Void
+
 class AppWKWebView: WKWebView {
   @available(*, unavailable)
   required init?(coder: NSCoder) {
@@ -12,5 +14,25 @@ class AppWKWebView: WKWebView {
 
   override var inputAccessoryView: UIView? {
     return nil
+  }
+
+  func setKeyboardRequiresUserInteraction(_ value: Bool) {
+    guard let WKContentView: AnyClass = NSClassFromString("WKContentView") else {
+      fatalError("Cannot find the WKContentView class")
+    }
+
+    let selector: Selector = sel_getUid("_elementDidFocus:userIsInteracting:blurPreviousNode:activityStateChanges:userObject:")
+
+    if let method = class_getInstanceMethod(WKContentView, selector) {
+      let imp: IMP = method_getImplementation(method)
+      let closure: ClosureType = unsafeBitCast(imp, to: ClosureType.self)
+
+      let block: @convention(block) (Any, UnsafeRawPointer, Bool, Bool, Bool, Any?) -> Void = { me, arg0, _, arg2, arg3, arg4 in
+        closure(me, selector, arg0, !value, arg2, arg3, arg4)
+      }
+
+      let override: IMP = imp_implementationWithBlock(block)
+      method_setImplementation(method, override)
+    }
   }
 }
