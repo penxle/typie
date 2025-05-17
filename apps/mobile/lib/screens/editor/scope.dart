a570850eca587bc85d14dce50d9d74029036a348
devunt <337728+devunt@users.noreply.github.com>
2025-05-17 04:54:09 +0000

    에디터 화면 리로드시 상태 유지 (#684)

--- apps/mobile/lib/screens/editor/scope.dart
+++ apps/mobile/lib/screens/editor/scope.dart
@@ -3,21 +3,30 @@ import 'package:typie/screens/editor/schema.dart';
 import 'package:typie/widgets/webview.dart';
 
 class EditorStateScope extends InheritedWidget {
-  EditorStateScope({required super.child, super.key});
+  const EditorStateScope({
+    required super.child,
+    required this.webViewController,
+    required this.proseMirrorState,
+    required this.keyboardHeight,
+    required this.isKeyboardVisible,
+    required this.selectedToolboxIdx,
+    required this.selectedTextbarIdx,
+    super.key,
+  });
 
-  final ValueNotifier<WebViewController?> webViewController = ValueNotifier(null);
-  final ValueNotifier<ProseMirrorState?> proseMirrorState = ValueNotifier(null);
-  final ValueNotifier<double> keyboardHeight = ValueNotifier(0);
-  final ValueNotifier<bool> isKeyboardVisible = ValueNotifier(false);
-  final ValueNotifier<int> selectedToolboxIdx = ValueNotifier(-1);
-  final ValueNotifier<int> selectedTextbarIdx = ValueNotifier(-1);
+  final ValueNotifier<WebViewController?> webViewController;
+  final ValueNotifier<ProseMirrorState?> proseMirrorState;
+  final ValueNotifier<double> keyboardHeight;
+  final ValueNotifier<bool> isKeyboardVisible;
+  final ValueNotifier<int> selectedToolboxIdx;
+  final ValueNotifier<int> selectedTextbarIdx;
 
   Future<void> command(String name, {Map<String, dynamic>? attrs}) async {
     await webViewController.value?.emitEvent('command', {'name': name, 'attrs': attrs});
   }
 
   static EditorStateScope of(BuildContext context) {
-    final scope = context.dependOnInheritedWidgetOfExactType<EditorStateScope>();
+    final scope = context.getInheritedWidgetOfExactType<EditorStateScope>();
     return scope!;
   }
 
