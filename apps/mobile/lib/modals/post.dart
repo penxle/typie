ea8b5eb0cfb210b83bcf68496b369b2e6211afe5
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2025-05-20 04:24:52 +0000

    포스트/폴더 모달에 기능 추가 (#707)

--- apps/mobile/lib/modals/post.dart
+++ apps/mobile/lib/modals/post.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:typie/context/full_screen_modal.dart';
+import 'package:typie/graphql/client.dart';
+import 'package:typie/hooks/service.dart';
+import 'package:typie/modals/__generated__/post_delete_post_mutation.req.gql.dart';
+import 'package:typie/modals/__generated__/post_duplicate_post_mutation.req.gql.dart';
 import 'package:typie/modals/move_entity.dart';
 import 'package:typie/screens/entity_tree/__generated__/entity_tree_post_post.data.gql.dart';
 import 'package:typie/widgets/btn.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PostModal extends HookWidget {
-  const PostModal({required this.post, super.key});
+  const PostModal(this.post, {super.key});
 
   final GEntityTree_Post_post post;
 
   @override
   Widget build(BuildContext context) {
+    final client = useService<GraphQLClient>();
+
     return Column(
       mainAxisSize: MainAxisSize.min,
       spacing: 8,
@@ -20,6 +27,25 @@ class PostModal extends HookWidget {
           '다른 폴더로 이동',
           onTap: () async {
             await context.showFullScreenModal(MoveEntityModal(post.entity.id));
+          },
+        ),
+        Btn(
+          '사이트에서 열기',
+          onTap: () async {
+            await launchUrl(Uri.parse(post.entity.url), mode: LaunchMode.externalApplication);
+          },
+        ),
+        Btn('공유', onTap: () {}),
+        Btn(
+          '복제',
+          onTap: () async {
+            await client.request(GPost_DuplicatePost_MutationReq((b) => b..vars.input.postId = post.id));
+          },
+        ),
+        Btn(
+          '삭제',
+          onTap: () async {
+            await client.request(GPost_DeletePost_MutationReq((b) => b..vars.input.postId = post.id));
           },
         ),
       ],
