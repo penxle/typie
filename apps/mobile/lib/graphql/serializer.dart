72b11229e77587add77755554a2c36287eadce36
devunt <337728+devunt@users.noreply.github.com>
2025-05-22 16:42:21 +0000

    엔티티 스크린 리뉴얼 (#729)

---
+++ apps/mobile/lib/graphql/serializer.dart
@@ -0,0 +1,25 @@
+import 'package:built_value/serializer.dart';
+import 'package:jiffy/jiffy.dart';
+
+class DateTimeSerializer implements PrimitiveSerializer<Jiffy> {
+  DateTimeSerializer() {
+    Jiffy.setLocale('ko_KR');
+  }
+
+  @override
+  Jiffy deserialize(Serializers serializers, Object serialized, {FullType specifiedType = FullType.unspecified}) {
+    assert(serialized is String, 'DateTimeSerializer expected String but got ${serialized.runtimeType}');
+    return Jiffy.parse(serialized as String);
+  }
+
+  @override
+  Object serialize(Serializers serializers, Jiffy object, {FullType specifiedType = FullType.unspecified}) {
+    return object.format();
+  }
+
+  @override
+  Iterable<Type> get types => [Jiffy];
+
+  @override
+  String get wireName => 'DateTime';
+}
