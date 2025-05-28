26a74795b0955934deb983582f8dab5e652200d1
devunt <337728+devunt@users.noreply.github.com>
2025-05-28 15:12:51 +0000

    플랜 관련 스크린 리뉴얼 (#788)

---
+++ apps/mobile/lib/screens/current_plan/screen.dart
@@ -0,0 +1,108 @@
+import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:typie/extensions/jiffy.dart';
+import 'package:typie/extensions/num.dart';
+import 'package:typie/graphql/widget.dart';
+import 'package:typie/icons/lucide_light.dart';
+import 'package:typie/routers/app.gr.dart';
+import 'package:typie/screens/current_plan/__generated__/screen.req.gql.dart';
+import 'package:typie/styles/colors.dart';
+import 'package:typie/widgets/heading.dart';
+import 'package:typie/widgets/horizontal_divider.dart';
+import 'package:typie/widgets/screen.dart';
+import 'package:typie/widgets/tappable.dart';
+import 'package:typie/widgets/vertical_divider.dart';
+
+@RoutePage()
+class CurrentPlanScreen extends StatelessWidget {
+  const CurrentPlanScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Screen(
+      heading: Heading(
+        title: '이용권 정보',
+        leadingWidget: HeadingLeading(
+          icon: LucideLightIcons.chevron_left,
+          onTap: () async {
+            await context.router.maybePop();
+          },
+        ),
+      ),
+      padding: const Pad(all: 20),
+      child: GraphQLOperation(
+        operation: GCurrentPlanScreen_QueryReq(),
+        builder: (context, client, data) {
+          return Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: AppColors.gray_950),
+              borderRadius: BorderRadius.circular(8),
+              color: AppColors.white,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const Pad(all: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const Text('현재 이용권', style: TextStyle(fontSize: 14, color: AppColors.gray_500)),
+                      const Gap(4),
+                      const Text('타이피 FULL ACCESS', style: TextStyle(fontWeight: FontWeight.w600)),
+                      const Gap(8),
+                      Text(
+                        '이용기간: ${data.me!.plan!.createdAt.yyyyMMdd} - ${data.me!.plan!.expiresAt.yyyyMMdd}',
+                        style: const TextStyle(fontSize: 14, color: AppColors.gray_500),
+                      ),
+                      Text(
+                        '다음 결제 예정일: ${data.me!.plan!.nextInvoice!.billingAt.yyyyMMdd}',
+                        style: const TextStyle(fontSize: 14, color: AppColors.gray_500),
+                      ),
+                      Text(
+                        '결제 예정 금액: ${data.me!.plan!.nextInvoice!.amount.comma}원',
+                        style: const TextStyle(fontSize: 14, color: AppColors.gray_500),
+                      ),
+                    ],
+                  ),
+                ),
+                const HorizontalDivider(color: AppColors.gray_950),
+                SizedBox(
+                  height: 48,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Tappable(
+                          onTap: () async {
+                            await context.router.push(const CancelPlanRoute());
+                          },
+                          child: const Center(
+                            child: Text('해지하기', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500)),
+                          ),
+                        ),
+                      ),
+                      const AppVerticalDivider(color: AppColors.gray_950),
+                      Expanded(
+                        child: Tappable(
+                          onTap: () async {
+                            await context.router.push(const EnrollPlanRoute());
+                          },
+                          child: const Center(
+                            child: Text('변경하기', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500)),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
