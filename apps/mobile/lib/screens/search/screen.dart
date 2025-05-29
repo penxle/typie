b08429be039b7dc615e91d41390131c21562ea9e
devunt <337728+devunt@users.noreply.github.com>
2025-05-29 05:31:00 +0000

    검색 스크린 placeholder (#798)

--- apps/mobile/lib/screens/search/screen.dart
+++ apps/mobile/lib/screens/search/screen.dart
@@ -1,5 +1,9 @@
+import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:typie/icons/lucide_light.dart';
+import 'package:typie/styles/colors.dart';
+import 'package:typie/widgets/heading.dart';
 import 'package:typie/widgets/screen.dart';
 
 @RoutePage()
@@ -8,6 +12,10 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Screen(child: Center(child: Text('search')));
+    return const Screen(
+      heading: Heading(title: '검색', titleIcon: LucideLightIcons.search),
+      padding: Pad(all: 20),
+      child: Placeholder(strokeWidth: 1, color: AppColors.gray_950),
+    );
   }
 }
