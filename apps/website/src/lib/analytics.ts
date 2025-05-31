def6ff8617039d549ab5eb83e2218d808ea59277
devunt <337728+devunt@users.noreply.github.com>
2025-05-31 09:14:06 +0000

    자잘한 코드 수정 (#833)

--- apps/website/src/lib/analytics.ts
+++ apps/website/src/lib/analytics.ts
@@ -3,6 +3,7 @@ import { env } from '$env/dynamic/public';
 
 export const setupMixpanel = () => {
   Mixpanel.init(env.PUBLIC_MIXPANEL_TOKEN, {
+    ignore_dnt: true,
     persistence: 'localStorage',
   });
 };
