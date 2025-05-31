d839dd640a3b64bf6a14e27018243c30fb3b28d3
devunt <337728+devunt@users.noreply.github.com>
2025-05-31 10:23:50 +0000

    k8s 걷어냄 (#834)
    잘있어 쿠버네티스
    함께해서 즐거웠고 나중에 또 보자

--- apps/infrastructure/pulumi/k8s/kube-system/metrics-server.ts
+++
@@ -1,9 +0,0 @@
-import * as k8s from '@pulumi/kubernetes';
-
-new k8s.helm.v4.Chart('metrics-server', {
-  chart: 'metrics-server',
-  namespace: 'kube-system',
-  repositoryOpts: {
-    repo: 'https://kubernetes-sigs.github.io/metrics-server',
-  },
-});
