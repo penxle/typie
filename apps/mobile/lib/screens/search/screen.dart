d716a13188fd4b9615ad22bd13ca2ed6ef860d2b
devunt <337728+devunt@users.noreply.github.com>
2025-06-02 15:02:28 +0000

    검색 스크린 리뉴얼 (#853)

--- apps/mobile/lib/screens/search/screen.dart
+++ apps/mobile/lib/screens/search/screen.dart
@@ -2,7 +2,7 @@ import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:typie/graphql/client.dart';
+import 'package:gap/gap.dart';
 import 'package:typie/graphql/widget.dart';
 import 'package:typie/hooks/service.dart';
 import 'package:typie/icons/lucide_light.dart';
@@ -22,107 +22,94 @@ class SearchScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final pref = useService<Pref>();
-    final client = useService<GraphQLClient>();
-
-    final result = useState<List<GSearchScreen_QueryData_search_hits>>([]);
+    final query = useState('');
 
     return Screen(
       heading: Heading(
-        title: '검색',
-        titleWidget: TextField(
-          autocorrect: false,
-          textAlignVertical: TextAlignVertical.top,
-          keyboardType: TextInputType.text,
-          smartDashesType: SmartDashesType.disabled,
-          decoration: const InputDecoration(
-            hintText: '검색어를 입력하세요',
-            hintStyle: TextStyle(color: AppColors.gray_500, fontWeight: FontWeight.w500),
-            fillColor: AppColors.gray_200,
-            filled: true,
-            border: InputBorder.none,
-          ),
-          onChanged: (value) async {
-            final res = await client.request(
-              GSearchScreen_QueryReq(
-                (b) => b
-                  ..vars.siteId = pref.siteId
-                  ..vars.query = value,
-              ),
-            );
-            result.value = res.search.hits.toList();
-          },
-        ),
         titleIcon: LucideLightIcons.search,
+        titleWidget: Padding(
+          padding: const Pad(left: 4),
+          child: TextField(
+            decoration: const InputDecoration.collapsed(
+              hintText: '검색어를 입력하세요',
+              hintStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: AppColors.gray_300),
+            ),
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            onChanged: (value) {
+              query.value = value;
+            },
+          ),
+        ),
       ),
-      padding: const Pad(all: 20),
       child: GraphQLOperation(
         operation: GSearchScreen_QueryReq(
           (b) => b
             ..vars.siteId = pref.siteId
-            ..vars.query = '',
+            ..vars.query = query.value,
         ),
         builder: (context, client, data) {
-          return result.value.isEmpty
-              ? const Center(
-                  child: Text('검색어를 입력해주세요', style: TextStyle(fontSize: 15, color: AppColors.gray_700)),
-                )
-              : ListView.builder(
-                  itemCount: result.value.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      contentPadding: Pad.zero,
-                      title: result.value[index].when(
-                        searchHitPost: (post) => Tappable(
-                          onTap: () async {
-                            await context.router.push(EditorRoute(slug: post.post.entity.slug));
-                          },
-                          child: Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(color: AppColors.gray_950),
-                              borderRadius: BorderRadius.circular(8),
-                              color: AppColors.white,
-                            ),
-                            padding: const Pad(all: 16),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    spacing: 4,
-                                    children: [
-                                      _HTMLText(
-                                        post.title ?? '(제목 없음)',
-                                        style: const TextStyle(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.w500,
-                                          color: AppColors.gray_950,
-                                        ),
-                                      ),
-                                      _HTMLText(
-                                        post.text ?? '(내용 없음)',
-                                        style: const TextStyle(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.w500,
-                                          color: AppColors.gray_950,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Text(
-                                  post.post.updatedAt.fromNow(),
-                                  style: const TextStyle(fontSize: 14, color: AppColors.gray_700),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        orElse: () => throw UnimplementedError(),
-                      ),
-                    );
+          if (query.value.isEmpty) {
+            return const Center(
+              child: Text('검색어를 입력해주세요', style: TextStyle(fontSize: 15, color: AppColors.gray_700)),
+            );
+          }
+
+          if (data.search.hits.isEmpty) {
+            return const Center(
+              child: Text('검색 결과가 없어요', style: TextStyle(fontSize: 15, color: AppColors.gray_700)),
+            );
+          }
+
+          return ListView.separated(
+            padding: const Pad(all: 20),
+            itemCount: data.search.hits.length,
+            itemBuilder: (context, index) {
+              return data.search.hits[index].when(
+                searchHitPost: (post) => Tappable(
+                  onTap: () async {
+                    await context.router.push(EditorRoute(slug: post.post.entity.slug));
                   },
-                );
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: AppColors.gray_950),
+                      borderRadius: BorderRadius.circular(8),
+                      color: AppColors.white,
+                    ),
+                    padding: const Pad(horizontal: 16, vertical: 12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      spacing: 4,
+                      children: [
+                        Row(
+                          spacing: 8,
+                          children: [
+                            Expanded(
+                              child: _HTMLText(
+                                post.title ?? '(제목 없음)',
+                                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                            Text(
+                              post.post.updatedAt.fromNow(),
+                              style: const TextStyle(fontSize: 14, color: AppColors.gray_700),
+                            ),
+                          ],
+                        ),
+                        _HTMLText(
+                          post.text ?? '(내용 없음)',
+                          style: const TextStyle(fontSize: 14, color: AppColors.gray_700),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                orElse: () => throw UnimplementedError(),
+              );
+            },
+            separatorBuilder: (context, index) {
+              return const Gap(12);
+            },
+          );
         },
       ),
     );
