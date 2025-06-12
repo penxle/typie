a3dca30369187f122a35aba6e6940b9ca32efd1c
devunt <337728+devunt@users.noreply.github.com>
2025-06-12 04:11:28 +0000

    firebase analytics 추가 (#962)

--- apps/mobile/lib/services/module.dart
+++ apps/mobile/lib/services/module.dart
@@ -2,6 +2,7 @@
 
 import 'package:dio/dio.dart';
 import 'package:dio_http2_adapter/dio_http2_adapter.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -13,6 +14,9 @@ import 'package:typie/styles/colors.dart';
 
 @module
 abstract class RegisterModule {
+  @singleton
+  FirebaseAnalytics get firebaseAnalytics => FirebaseAnalytics.instance;
+
   @singleton
   FlutterSecureStorage get flutterSecureStorage =>
       const FlutterSecureStorage(aOptions: AndroidOptions(encryptedSharedPreferences: true, resetOnError: true));
