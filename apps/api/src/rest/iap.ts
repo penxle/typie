833bb4818c2d1b5b3867b8f6e63e38a3174d0352
robin-maki <8201350+robin-maki@users.noreply.github.com>
2025-06-18 09:31:49 +0000

    앱스토어 인앱 웹훅 처리 (#996)

--- apps/api/src/rest/iap.ts
+++ apps/api/src/rest/iap.ts
@@ -1,4 +1,9 @@
+import dayjs from 'dayjs';
+import { and, eq, inArray } from 'drizzle-orm';
 import { Hono } from 'hono';
+import { match } from 'ts-pattern';
+import { db, first, firstOrThrow, Plans, Subscriptions, UserInAppPurchases } from '@/db';
+import { InAppPurchaseStore, PlanAvailability, SubscriptionState } from '@/enums';
 import * as appstore from '@/external/appstore';
 import * as googleplay from '@/external/googleplay';
 import * as slack from '@/external/slack';
@@ -15,8 +20,91 @@ iap.post('/appstore', async (c) => {
   }
 
   const notification = await appstore.decodeNotification(body.signedPayload);
+  const originalTransactionId = notification.data.transaction?.originalTransactionId;
+  const planId = notification.data.transaction?.productId?.toUpperCase();
 
-  await slack.sendMessage({ channel: 'iap', message: JSON.stringify({ source: 'rest/appstore', notification }, null, 2) });
+  if (!originalTransactionId || !planId) {
+    return c.json({ error: 'invalid_request' }, 400);
+  }
+
+  const { userId } = await db
+    .select({
+      userId: UserInAppPurchases.userId,
+    })
+    .from(UserInAppPurchases)
+    .where(and(eq(UserInAppPurchases.identifier, originalTransactionId), eq(UserInAppPurchases.store, InAppPurchaseStore.APP_STORE)))
+    .then(firstOrThrow);
+
+  const subscription = await db
+    .select({
+      id: Subscriptions.id,
+      state: Subscriptions.state,
+      expiresAt: Subscriptions.expiresAt,
+    })
+    .from(Subscriptions)
+    .innerJoin(Plans, eq(Subscriptions.planId, Plans.id))
+    .where(
+      and(
+        eq(Subscriptions.userId, userId),
+        eq(Plans.availability, PlanAvailability.IN_APP_PURCHASE),
+        inArray(Subscriptions.state, [SubscriptionState.ACTIVE, SubscriptionState.WILL_EXPIRE, SubscriptionState.IN_GRACE_PERIOD]),
+      ),
+    )
+    .then(first);
+
+  await match(notification.notificationType)
+    .with('DID_RENEW', 'SUBSCRIBED', async () => {
+      if (subscription) {
+        await db
+          .update(Subscriptions)
+          .set({
+            state: SubscriptionState.ACTIVE,
+            expiresAt: dayjs(notification.data.transaction?.expiresDate),
+          })
+          .where(eq(Subscriptions.id, subscription.id));
+      } else {
+        await db.insert(Subscriptions).values({
+          userId,
+          planId,
+          startsAt: dayjs(notification.data.transaction?.purchaseDate),
+          expiresAt: dayjs(notification.data.transaction?.expiresDate),
+          state: SubscriptionState.ACTIVE,
+        });
+      }
+    })
+    .with('EXPIRED', 'GRACE_PERIOD_EXPIRED', async () => {
+      if (subscription) {
+        await db.update(Subscriptions).set({ state: SubscriptionState.EXPIRED }).where(eq(Subscriptions.id, subscription.id));
+      }
+    })
+    .with('DID_CHANGE_RENEWAL_PREF', async () => {
+      if (subscription) {
+        await db
+          .update(Subscriptions)
+          .set({ planId, expiresAt: dayjs(notification.data.transaction?.expiresDate) })
+          .where(eq(Subscriptions.id, subscription.id));
+      }
+    })
+    .with('DID_CHANGE_RENEWAL_STATUS', async () => {
+      if (subscription) {
+        if (notification.subtype === 'AUTO_RENEW_DISABLED') {
+          await db.update(Subscriptions).set({ state: SubscriptionState.WILL_EXPIRE }).where(eq(Subscriptions.id, subscription.id));
+        } else if (notification.subtype === 'AUTO_RENEW_ENABLED' && subscription.state === SubscriptionState.WILL_EXPIRE) {
+          await db.update(Subscriptions).set({ state: SubscriptionState.ACTIVE }).where(eq(Subscriptions.id, subscription.id));
+        }
+      }
+    })
+    .with('DID_FAIL_TO_RENEW', async () => {
+      if (subscription) {
+        await db
+          .update(Subscriptions)
+          .set({ state: notification.subtype === 'GRACE_PERIOD' ? SubscriptionState.IN_GRACE_PERIOD : SubscriptionState.EXPIRED })
+          .where(eq(Subscriptions.id, subscription.id));
+      }
+    })
+    .otherwise(async () => {
+      await slack.sendMessage({ channel: 'iap', message: JSON.stringify({ source: 'rest/appstore', notification }, null, 2) });
+    });
 
   return c.json({}, 200);
 });
