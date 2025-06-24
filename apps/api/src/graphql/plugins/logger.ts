3afcab77e4dce1b281a05f7927c2f89ce59060ed
devunt <337728+devunt@users.noreply.github.com>
2025-06-24 05:15:24 +0000

    graphql child logger 이용 (#1061)

--- apps/api/src/graphql/plugins/logger.ts
+++ apps/api/src/graphql/plugins/logger.ts
@@ -2,9 +2,11 @@ import { logger } from '@typie/lib';
 import type { Plugin } from 'graphql-yoga';
 import type { Context } from '@/context';
 
+const log = logger.getChild('graphql');
+
 export const useLogger = (): Plugin<Context> => ({
   onExecute: ({ args }) => {
-    logger.info('Executed operation {*}', {
+    log.info('Executed operation {*}', {
       operationName: args.operationName,
       ip: args.contextValue.ip,
       userId: args.contextValue.session?.userId,
