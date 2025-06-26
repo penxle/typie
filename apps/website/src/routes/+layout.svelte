4d86ae080d00ceb769274233f65a420a65dbe878
devunt <337728+devunt@users.noreply.github.com>
2025-06-26 13:27:32 +0000

    테마 변경에 view transition 이용함 (#1089)

--- apps/website/src/routes/+layout.svelte
+++ apps/website/src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import '../app.css';
 
+  import { setupThemeContext } from '$lib/context';
   import { NotificationProvider } from '$lib/notification';
 
   let { children } = $props();
+
+  setupThemeContext();
 </script>
 
 {@render children()}
