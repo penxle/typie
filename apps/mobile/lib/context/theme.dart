ab2a544f7afdc7fbcbb19aebcf43db2176968746
devunt <337728+devunt@users.noreply.github.com>
2025-06-28 09:52:47 +0000

    안드로이드 edge to edge 모드 적용 (#1119)

--- apps/mobile/lib/context/theme.dart
+++ apps/mobile/lib/context/theme.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:typie/styles/semantic_colors.dart';
 
 extension BuildContextExtensions on BuildContext {
+  ThemeData get theme => Theme.of(this);
   SemanticColors get colors => Theme.of(this).extension<SemanticColors>()!;
 }
--- apps/mobile/lib/services/static.dart
+++ apps/mobile/lib/services/static.dart
@@ -1,11 +1,27 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 import 'package:jiffy/jiffy.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:naver_login_sdk/naver_login_sdk.dart';
 import 'package:typie/env.dart';
 import 'package:typie/firebase_options.dart';
+import 'package:typie/styles/colors.dart';
 
 Future<void> configureStaticServices() async {
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: AppColors.transparent,
+      statusBarBrightness: Brightness.light,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: AppColors.transparent,
+      systemNavigationBarDividerColor: AppColors.transparent,
+      systemNavigationBarIconBrightness: Brightness.dark,
+      systemStatusBarContrastEnforced: false,
+      systemNavigationBarContrastEnforced: false,
+    ),
+  );
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   KakaoSdk.init(nativeAppKey: Env.kakaoNativeAppKey);
