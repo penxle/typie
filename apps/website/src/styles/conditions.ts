5e9d1ce79934fc0e68a885a37ab4211f09da8a8e
devunt <337728+devunt@users.noreply.github.com>
2025-07-22 08:46:13 +0000

    랜딩 모바일 반응형 도입 (#1299)

--- apps/website/src/styles/conditions.ts
+++ apps/website/src/styles/conditions.ts
@@ -2,8 +2,9 @@ export const conditions = {
   extend: {
     enabled: '&:is(:enabled, a[aria-disabled="false"])',
     disabled: '&:is(:disabled, [aria-disabled="true"])',
-    hover: '&:hover:not([aria-pressed="true"])',
+    hover: ['@media (hover: hover) and (pointer: fine)', '&:hover:not([aria-pressed="true"])'],
     supportHover: ['@media (hover: hover) and (pointer: fine)', '&:hover'],
+    active: ['@media (hover: hover) and (pointer: fine)', '&:active'],
     dark: '[data-theme="dark"] &',
   },
 };
