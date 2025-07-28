197b83049920a43374752d9563754ea9663b1f41
devunt <337728+devunt@users.noreply.github.com>
2025-07-28 04:03:56 +0000

    웹사이트 sitemap 갱신 (#1399)

--- apps/website/src/routes/website/sitemap.xml/+server.ts
+++ apps/website/src/routes/website/sitemap.xml/+server.ts
@@ -2,5 +2,5 @@ import { sitemap } from '@typie/lib/svelte';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async (event) => {
-  return sitemap(event, ['/']);
+  return sitemap(event, ['/', '/pricing', '/changelog']);
 };
