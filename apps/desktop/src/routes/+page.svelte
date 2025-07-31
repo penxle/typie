b10f7110724c9e838c27333dedd164d09db8b87b
devunt <337728+devunt@users.noreply.github.com>
2025-07-31 08:32:03 +0000

    desktop (#1446)

---
+++ apps/desktop/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<main class="container">
+  <p>짠</p>
+</main>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+</style>
