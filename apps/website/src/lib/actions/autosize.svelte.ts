c0f5b00fc1fa0dde26a52808cb180c44016776be
dolsup <8360934+dolsup@users.noreply.github.com>
2025-08-04 03:13:16 +0000

    autosize 액션 수정 (#1474)
    - 제목 두 줄일 때 스크롤바 생기는 문제 해결
    - 첫 로드 후 입력 전까지 1줄로만 보이는 문제 해결

--- apps/website/src/lib/actions/autosize.svelte.ts
+++ apps/website/src/lib/actions/autosize.svelte.ts
@@ -3,13 +3,18 @@ import type { Action } from 'svelte/action';
 
 export const autosize: Action<HTMLTextAreaElement> = (element) => {
   $effect(() => {
+    element.style.overflow = 'hidden';
+
     const handler = () => {
       element.style.height = 'auto';
       element.style.height = `${element.scrollHeight}px`;
     };
 
     const off = on(element, 'input', handler);
-    handler();
+
+    requestAnimationFrame(() => {
+      handler();
+    });
 
     return () => {
       off();
