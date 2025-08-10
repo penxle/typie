7709861885bb61cb3f302ce7fa4f89ec02762c9f
Bae Junehyeon <finn@penxle.io>
2025-08-10 18:55:47 +0900

    desktop vite 설정 추가 (#1570)

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -1,11 +1,26 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { svg } from '@typie/lib/vite';
+import { sark } from '@typie/sark/vite';
+import { FileSystemIconLoader } from 'unplugin-icons/loaders';
+import icons from 'unplugin-icons/vite';
 import { defineConfig } from 'vite';
 
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig({
   clearScreen: false,
-  plugins: [sveltekit()],
+  plugins: [
+    svg(),
+    icons({
+      scale: 1,
+      compiler: 'svelte',
+      customCollections: {
+        typie: FileSystemIconLoader('./src/icons'),
+      },
+    }),
+    sark(),
+    sveltekit(),
+  ],
   server: {
     port: 5000,
     strictPort: true,
