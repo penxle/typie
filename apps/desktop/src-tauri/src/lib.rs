80a3a91581d5f12c0426aa3a5ba1ffb62f19fb06
Bae Junehyeon <finn@penxle.io>
2025-08-10 18:56:02 +0900

    데스크탑 딥링크 설정 (#1571)

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -1,6 +1,14 @@
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
+use tauri::Manager;
+
 pub fn run() {
   tauri::Builder::default()
+    .plugin(tauri_plugin_single_instance::init(|app, _, _| {
+      let _ = app
+        .get_webview_window("main")
+        .expect("no main window")
+        .set_focus();
+    }))
+    .plugin(tauri_plugin_deep_link::init())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
