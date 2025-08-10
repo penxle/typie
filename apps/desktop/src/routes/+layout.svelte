0c771906a034c3976e7bea8ba37ad0b9970ea733
Bae Junehyeon <devunt@gmail.com>
2025-08-10 18:51:42 +0900

    데스크탑 딥링크 설정

--- apps/desktop/src/routes/+layout.svelte
+++ apps/desktop/src/routes/+layout.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
   import '../app.css';
 
+  import { onOpenUrl } from '@tauri-apps/plugin-deep-link';
   import { css } from '@typie/styled-system/css';
+  import { onMount } from 'svelte';
 
   let { children } = $props();
+
+  onMount(() => {
+    onOpenUrl((urls) => {
+      console.log('Deep links received:', urls);
+    });
+  });
 </script>
 
 <div class={css({ position: 'relative', width: '[100vw]', height: '[100vh]', paddingTop: '36px' })}>
