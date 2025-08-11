f9a2873a970f38f3942b924d5ae5b0c6e65ffbe5
Bae Junehyeon <finn@penxle.io>
2025-08-11 22:14:29 +0900

    데스크탑 앱 로그인 2 (#1590)

---
+++ apps/desktop/src/routes/(app)/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { graphql } from '$graphql';
+
+  const { children } = $props();
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const query = graphql(`
+    query Layout_Query {
+      me {
+        id
+      }
+    }
+  `);
+</script>
+
+{@render children()}
