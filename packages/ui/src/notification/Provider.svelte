69615c2464b585520a4f21f779c77be14ba2a500
Seohyun Roh <kylie@penxle.io>
2025-08-13 17:23:05 +0900

    tip도 svelte-sonner로 띄움 (#1602)

--- packages/ui/src/notification/Provider.svelte
+++ packages/ui/src/notification/Provider.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
   import DialogProvider from './dialog/Provider.svelte';
   import SonnerProvider from './sonner/Provider.svelte';
-  import TipProvider from './tip/Provider.svelte';
 </script>
 
 <DialogProvider />
-<TipProvider />
 <SonnerProvider />
