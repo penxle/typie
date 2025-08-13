a7777a494fa8be74aef97169e7f6fcb3eb9a108b
kylie <kylie@penxle.io>
2025-08-13 17:16:13 +0900

    tip도 svelte-sonner로 띄움

--- packages/ui/src/notification/index.ts
+++ packages/ui/src/notification/index.ts
@@ -1,4 +1,4 @@
 export { dialog as Dialog } from './dialog/store';
 export { default as NotificationProvider } from './Provider.svelte';
-export { tip as Tip } from './tip/store';
+export { tip as Tip } from './tip';
 export { toast as Toast } from './toast';
