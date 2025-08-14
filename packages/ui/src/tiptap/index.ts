f0f599110e5778b24112b9b903d92f86a6cdacf0
Jiwon Choi <1890mah@gmail.com>
2025-08-14 13:24:10 +0900

    웹뷰 행렬 핸들에서 표 삭제 메뉴 없애기 (#1611)

--- packages/ui/src/tiptap/index.ts
+++ packages/ui/src/tiptap/index.ts
@@ -3,4 +3,5 @@ export * from './extensions/collaboration';
 export * from './extensions/page';
 export * from './lib';
 export * from './schema';
+export * from './types';
 export * from './values';
