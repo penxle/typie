5ff82e063c97579e65b7ed681aeb0f748fcae0a1
Jiwon Choi <1890mah@gmail.com>
2025-08-17 22:32:51 +0900

    앱 액티비티 그리드 KST 사용

--- apps/mobile/lib/screens/profile/activity_grid.dart
+++ apps/mobile/lib/screens/profile/activity_grid.dart
@@ -415,8 +415,9 @@ class ActivityGrid extends HookWidget {
     final changesMap = <String, GProfileScreen_QueryData_me_characterCountChanges>{};
 
     for (final change in characterCountChanges) {
-      final key = change.date.format(pattern: 'yyyy-MM-dd');
-      changesMap[key] = change;
+      final kstDate = change.date.add(hours: 9);
+      final dateKey = kstDate.format(pattern: 'yyyy-MM-dd');
+      changesMap[dateKey] = change;
     }
 
     final numbers = characterCountChanges.map((c) => c.additions).where((n) => n > 0).toList();
