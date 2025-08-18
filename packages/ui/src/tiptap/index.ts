93eb83649134a746c6f1ad43655ea45f4c01b969
Jiwon Choi <1890mah@gmail.com>
2025-08-18 20:09:04 +0900

    PDF export 시 CSR만 하고 렌더링 idle 상태 기다림

--- packages/ui/src/tiptap/index.ts
+++ packages/ui/src/tiptap/index.ts
@@ -1,5 +1,6 @@
 export * from './components';
 export * from './extensions/collaboration';
+export * from './extensions/notify-idle';
 export * from './extensions/page';
 export * from './lib';
 export * from './schema';
