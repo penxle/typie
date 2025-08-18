86f40a30f1307d70819e296788b08047e59c7088
Bae Junehyeon <finn@penxle.io>
2025-08-18 13:01:08 +0900

    Img 컴포넌트 코드 중복 정리 (#1656)

--- apps/website/src/lib/components/Img.svelte
+++ apps/website/src/lib/components/Img.svelte
@@ -1,10 +1,5 @@
 <script lang="ts">
-  import { css } from '@typie/styled-system/css';
-  import qs from 'query-string';
-  import { base64 } from 'rfc4648';
-  import { tick } from 'svelte';
-  import { fade } from 'svelte/transition';
-  import { thumbHashToDataURL } from 'thumbhash';
+  import { Img } from '@typie/ui/components';
   import { fragment, graphql } from '$graphql';
   import type { SystemStyleObject } from '@typie/styled-system/types';
   import type { HTMLImgAttributes } from 'svelte/elements';
@@ -20,9 +15,9 @@
     ratio?: number;
     quality?: number;
     progressive?: boolean;
-  } & Omit<HTMLImgAttributes, 'style' | 'src' | 'srcset' | 'sizes' | 'alt'>;
+  } & Omit<HTMLImgAttributes, 'style' | 'src' | 'srcset' | 'sizes' | 'alt' | 'placeholder'>;
 
-  let { $image: _image, alt, style, size, ratio, quality, progressive = false, ...rest }: Props = $props();
+  let { $image: _image, ...rest }: Props = $props();
 
   const image = fragment(
     _image,
@@ -35,89 +30,6 @@
       }
     `),
   );
-
-  let containerEl = $state<HTMLElement>();
-  let targetEl = $state<HTMLElement>();
-
-  let loaded = $state(false);
-
-  const src = $derived(qs.stringifyUrl({ url: $image.url, query: { s: size === 'full' ? undefined : size, q: quality } }));
-  const src2x = $derived(size !== 'full' && qs.stringifyUrl({ url: $image.url, query: { s: size * 2, q: quality } }));
-  const src3x = $derived(size !== 'full' && qs.stringifyUrl({ url: $image.url, query: { s: size * 3, q: quality } }));
-
-  const sizes = $derived(size === 'full' ? undefined : `${size}px`);
-  const srcset = $derived(size === 'full' ? undefined : `${src} ${size}w, ${src2x} ${size * 2}w, ${src3x} ${size * 3}w`);
-
-  const placeholderUrl = $derived(progressive ? thumbHashToDataURL(base64.parse($image.placeholder)) : undefined);
-
-  const load = () => {
-    if (loaded) {
-      return;
-    }
-
-    const imgEl = document.createElement('img');
-
-    imgEl.addEventListener('load', async () => {
-      loaded = true;
-      await tick();
-      // eslint-disable-next-line svelte/no-dom-manipulating
-      targetEl?.append(imgEl);
-    });
-
-    if (srcset && sizes) {
-      imgEl.sizes = sizes;
-      imgEl.srcset = srcset;
-    }
-
-    imgEl.className = css({ size: 'full', objectFit: 'cover' }, style);
-    imgEl.alt = alt;
-    imgEl.src = src;
-
-    Object.assign(imgEl, rest);
-  };
-
-  $effect(() => {
-    if (containerEl) {
-      loaded = false;
-
-      const observer = new IntersectionObserver((entries) => {
-        if (entries.some((entry) => entry.isIntersecting)) {
-          load();
-          observer.disconnect();
-        }
-      });
-
-      observer.observe(containerEl);
-
-      return () => {
-        observer.disconnect();
-      };
-    }
-  });
 </script>
 
-{#key $image.url}
-  {#if placeholderUrl}
-    <div
-      bind:this={containerEl}
-      style:aspect-ratio={ratio ?? $image.ratio}
-      class={css({
-        position: 'relative',
-        width: 'full',
-        overflow: 'hidden',
-      })}
-    >
-      <img class={css(style, { size: 'full', objectFit: 'cover' })} {alt} loading="lazy" src={placeholderUrl} {...rest} />
-
-      {#if loaded}
-        <div
-          bind:this={targetEl}
-          class={css({ position: 'absolute', inset: '0', backgroundColor: 'surface.default' })}
-          in:fade={{ duration: 200 }}
-        ></div>
-      {/if}
-    </div>
-  {:else}
-    <img class={css(style)} {alt} loading="lazy" {sizes} {src} {srcset} {...rest} />
-  {/if}
-{/key}
+<Img placeholder={$image.placeholder} url={$image.url} {...rest} />
