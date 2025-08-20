bec54e76cfc309cadb1a2c711757654e81911666
Jiwon Choi <1890mah@gmail.com>
2025-08-20 17:12:38 +0900

    앱 에디터 placeholder 포지셔닝 디버그 (#1705)

--- apps/website/src/routes/website/_webview/editor/Placeholder.svelte
+++ apps/website/src/routes/website/_webview/editor/Placeholder.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { css } from '@typie/styled-system/css';
-  import { center, flex } from '@typie/styled-system/patterns';
+  import { flex } from '@typie/styled-system/patterns';
   import { Icon } from '@typie/ui/components';
   import { isBodyEmpty } from '@typie/ui/tiptap';
   import ShapesIcon from '~icons/lucide/shapes';
@@ -24,39 +24,38 @@
 </script>
 
 {#if emptyBody}
-  <div class={center({ position: 'absolute', top: '0', insetX: '0', flexGrow: '1', pointerEvents: 'none' })}>
-    <div
-      style:padding-left={`${paragraphIndent}em`}
-      class={flex({
-        flexDirection: 'column',
-        gap: '4px',
-        width: 'full',
-        maxWidth: 'var(--prosemirror-max-width)',
-        color: 'text.disabled',
-        lineHeight: '[1.6]',
-      })}
-    >
-      <div class={css({ fontFamily: 'ui' })}>내용</div>
+  <div
+    style:padding-left={`${paragraphIndent}em`}
+    class={flex({
+      flexDirection: 'column',
+      gap: '4px',
+      width: 'full',
+      maxWidth: 'var(--prosemirror-max-width)',
+      color: 'text.disabled',
+      lineHeight: '[1.6]',
+      pointerEvents: 'none',
+    })}
+  >
+    <div class={css({ fontFamily: 'ui' })}>내용</div>
 
-      {#if isTemplateActive}
-        <div class={flex({ alignItems: 'center', gap: '4px' })}>
-          <div>혹은</div>
-          <button
-            class={flex({
-              alignItems: 'center',
-              gap: '4px',
-              pointerEvents: 'auto',
-            })}
-            onclick={() => {
-              window.__webview__?.emitEvent('useTemplate');
-            }}
-            type="button"
-          >
-            <Icon icon={ShapesIcon} size={16} />
-            <div>템플릿 사용하기</div>
-          </button>
-        </div>
-      {/if}
-    </div>
+    {#if isTemplateActive}
+      <div class={flex({ alignItems: 'center', gap: '4px' })}>
+        <div>혹은</div>
+        <button
+          class={flex({
+            alignItems: 'center',
+            gap: '4px',
+            pointerEvents: 'auto',
+          })}
+          onclick={() => {
+            window.__webview__?.emitEvent('useTemplate');
+          }}
+          type="button"
+        >
+          <Icon icon={ShapesIcon} size={16} />
+          <div>템플릿 사용하기</div>
+        </button>
+      </div>
+    {/if}
   </div>
 {/if}
