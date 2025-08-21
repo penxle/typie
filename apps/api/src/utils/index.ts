3db0345071bbba3fe5e4c68cffe9e211f3243cb3
Bae Junehyeon <finn@penxle.io>
2025-08-21 21:40:41 +0900

    Revert "worker concurrency 최적화" (#1732)
    Reverts penxle/typie#1731

--- apps/api/src/utils/index.ts
+++ apps/api/src/utils/index.ts
@@ -2,7 +2,6 @@ export * from './auth';
 export * from './date';
 export * from './entity';
 export * from './image-generation';
-export * from './lazy';
 export * from './name';
 export * from './payment';
 export * from './text';
