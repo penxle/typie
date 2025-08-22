786773a7577ea8525ae0b26504b3ebe2910d1ea6
Jiwon Choi <1890mah@gmail.com>
2025-08-22 17:41:54 +0900

    PDF 내보내기 폰트 (#1743)

--- apps/website/src/routes/website/_internal/export/pdf/[slug]/+page.ts
+++ apps/website/src/routes/website/_internal/export/pdf/[slug]/+page.ts
@@ -1,5 +1,12 @@
 import type { ExportPdfSlugPage_query_Variables } from './$graphql';
 
+declare global {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface Window {
+    notifyFontsReady?: () => void;
+  }
+}
+
 export const _ExportPdfSlugPage_query_Variables: ExportPdfSlugPage_query_Variables = ({ params }) => ({
   slug: params.slug,
 });
