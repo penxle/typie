6dad5d4432004aba7fadd0e7cbebc9eac85dca32
Bae Junehyeon <finn@penxle.io>
2025-09-01 14:32:02 +0900

    gql_tristate_value 도입 (#1862)

--- apps/mobile/lib/screens/update_password/screen.dart
+++ apps/mobile/lib/screens/update_password/screen.dart
@@ -2,6 +2,7 @@ import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:gql_tristate_value/gql_tristate_value.dart';
 import 'package:luthor/luthor.dart';
 import 'package:mixpanel_flutter/mixpanel_flutter.dart';
 import 'package:typie/context/toast.dart';
@@ -61,7 +62,7 @@ class UpdatePasswordScreen extends HookWidget {
                 await client.request(
                   GUpdatePasswordScreen_UpdatePassword_MutationReq(
                     (b) => b
-                      ..vars.input.currentPassword = hasPassword ? currentPassword : null
+                      ..vars.input.currentPassword = hasPassword ? Value.present(currentPassword) : const Value.absent()
                       ..vars.input.newPassword = newPassword,
                   ),
                 );
