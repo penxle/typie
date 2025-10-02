3aeb8f6b84a26dfe2aa8ee08a023fb2435dc42d4
devunt <337728+devunt@users.noreply.github.com>
2025-10-02 06:43:56 +0000

    rust fmt 실행 (#2119)

--- crates/fondue/build.rs
+++ crates/fondue/build.rs
@@ -1,34 +1,34 @@
 fn main() {
-  println!("cargo:rerun-if-changed=src/wrapper.h");
-  println!("cargo:rerun-if-changed=src/wrapper.cpp");
+    println!("cargo:rerun-if-changed=src/wrapper.h");
+    println!("cargo:rerun-if-changed=src/wrapper.cpp");
 
-  let woff2 = glob::glob("vendor/woff2/src/*.cc").unwrap();
-  let brotli = glob::glob("vendor/brotli/c/**/*.c").unwrap();
+    let woff2 = glob::glob("vendor/woff2/src/*.cc").unwrap();
+    let brotli = glob::glob("vendor/brotli/c/**/*.c").unwrap();
 
-  let mut build = cc::Build::new();
+    let mut build = cc::Build::new();
 
-  build
-    .cpp(true)
-    .warnings(false)
-    .flag_if_supported("-Wno-deprecated")
-    .include("vendor/woff2/include")
-    .include("vendor/brotli/c/include");
+    build
+        .cpp(true)
+        .warnings(false)
+        .flag_if_supported("-Wno-deprecated")
+        .include("vendor/woff2/include")
+        .include("vendor/brotli/c/include");
 
-  for file in woff2 {
-    build.file(file.unwrap());
-  }
+    for file in woff2 {
+        build.file(file.unwrap());
+    }
 
-  for file in brotli {
-    build.file(file.unwrap());
-  }
+    for file in brotli {
+        build.file(file.unwrap());
+    }
 
-  build.compile("woff2");
+    build.compile("woff2");
 
-  cxx_build::bridge("src/lib.rs")
-    .file("src/wrapper.cpp")
-    .include("src")
-    .include("vendor/woff2/include")
-    .compile("woff2-bridge");
+    cxx_build::bridge("src/lib.rs")
+        .file("src/wrapper.cpp")
+        .include("src")
+        .include("vendor/woff2/include")
+        .compile("woff2-bridge");
 
-  napi_build::setup();
+    napi_build::setup();
 }
--- crates/fondue/src/lib.rs
+++ crates/fondue/src/lib.rs
@@ -4,89 +4,88 @@ use ttf_parser::{Face, Language, Style, name_id};
 
 #[cxx::bridge]
 mod ffi {
-  unsafe extern "C++" {
-    include!("wrapper.h");
+    unsafe extern "C++" {
+        include!("wrapper.h");
 
-    fn compress_woff2(data: &[u8]) -> Vec<u8>;
-  }
+        fn compress_woff2(data: &[u8]) -> Vec<u8>;
+    }
 }
 
 #[napi(object)]
 pub struct FontMetadata {
-  pub weight: u16,
-  #[napi(ts_type = "'normal' | 'italic' | 'oblique'")]
-  pub style: String,
-  pub family_name: Option<String>,
-  pub full_name: Option<String>,
-  pub post_script_name: Option<String>,
+    pub weight: u16,
+    #[napi(ts_type = "'normal' | 'italic' | 'oblique'")]
+    pub style: String,
+    pub family_name: Option<String>,
+    pub full_name: Option<String>,
+    pub post_script_name: Option<String>,
 }
 
 fn get_name(face: &Face, name_id: u16) -> Option<String> {
-  let languages = [
-    Some(Language::Korean_Korea),
-    Some(Language::English_UnitedStates),
-    None,
-  ];
+    let languages = [
+        Some(Language::Korean_Korea),
+        Some(Language::English_UnitedStates),
+        None,
+    ];
 
-  for &language in &languages {
-    let result = face
-      .names()
-      .into_iter()
-      .find(|name| {
-        if let Some(language) = language {
-          name.language() == language && name.name_id == name_id
-        } else {
-          name.name_id == name_id
+    for &language in &languages {
+        let result = face
+            .names()
+            .into_iter()
+            .find(|name| {
+                if let Some(language) = language {
+                    name.language() == language && name.name_id == name_id
+                } else {
+                    name.name_id == name_id
+                }
+            })
+            .and_then(|name| {
+                name.to_string()
+                    .or_else(|| String::from_utf8(name.name.to_vec()).ok())
+            });
+
+        if result.is_some() {
+            return result;
         }
-      })
-      .and_then(|name| {
-        name
-          .to_string()
-          .or_else(|| String::from_utf8(name.name.to_vec()).ok())
-      });
-
-    if result.is_some() {
-      return result;
     }
-  }
 
-  None
+    None
 }
 
 #[napi]
 pub fn get_font_metadata(data: Uint8Array) -> Result<FontMetadata> {
-  let face = Face::parse(data.as_ref(), 0).map_err(|err| Error::from_reason(err.to_string()))?;
+    let face = Face::parse(data.as_ref(), 0).map_err(|err| Error::from_reason(err.to_string()))?;
 
-  let weight = face.weight().to_number();
-  let style = match face.style() {
-    Style::Normal => "normal",
-    Style::Italic => "italic",
-    Style::Oblique => "oblique",
-  }
-  .to_string();
+    let weight = face.weight().to_number();
+    let style = match face.style() {
+        Style::Normal => "normal",
+        Style::Italic => "italic",
+        Style::Oblique => "oblique",
+    }
+    .to_string();
 
-  let family_name =
-    get_name(&face, name_id::TYPOGRAPHIC_FAMILY).or_else(|| get_name(&face, name_id::FAMILY));
-  let full_name = get_name(&face, name_id::FULL_NAME);
-  let post_script_name = get_name(&face, name_id::POST_SCRIPT_NAME);
+    let family_name =
+        get_name(&face, name_id::TYPOGRAPHIC_FAMILY).or_else(|| get_name(&face, name_id::FAMILY));
+    let full_name = get_name(&face, name_id::FULL_NAME);
+    let post_script_name = get_name(&face, name_id::POST_SCRIPT_NAME);
 
-  Ok(FontMetadata {
-    weight,
-    style,
-    family_name,
-    full_name,
-    post_script_name,
-  })
+    Ok(FontMetadata {
+        weight,
+        style,
+        family_name,
+        full_name,
+        post_script_name,
+    })
 }
 
 #[napi]
 pub fn to_woff2(data: Uint8Array) -> Result<Uint8Array> {
-  let input = data.as_ref();
-  let output = ffi::compress_woff2(input);
+    let input = data.as_ref();
+    let output = ffi::compress_woff2(input);
 
-  if output.is_empty() {
-    Err(Error::from_reason("Failed to convert font to WOFF2 format"))
-  } else {
-    Ok(output.into())
-  }
+    if output.is_empty() {
+        Err(Error::from_reason("Failed to convert font to WOFF2 format"))
+    } else {
+        Ok(output.into())
+    }
 }
