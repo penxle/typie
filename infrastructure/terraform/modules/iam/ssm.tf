8010f3966f1d356325b0b2b49cae87664d385d7d
devunt <337728+devunt@users.noreply.github.com>
2025-10-05 13:04:46 +0000

    iam ssm 파라메터 타입 및 경로 변경 (#2151)

--- infrastructure/terraform/modules/iam/ssm.tf
+++ infrastructure/terraform/modules/iam/ssm.tf
@@ -1,47 +1,47 @@
 resource "aws_ssm_parameter" "external_secrets_aws_access_key_id" {
-  name  = "/external_secrets/aws_access_key_id"
-  type  = "SecureString"
+  name  = "/iam/external_secrets/aws_access_key_id"
+  type  = "String"
   value = aws_iam_access_key.external_secrets.id
 }
 
 resource "aws_ssm_parameter" "external_secrets_aws_secret_access_key" {
-  name  = "/external_secrets/aws_secret_access_key"
-  type  = "SecureString"
+  name  = "/iam/external_secrets/aws_secret_access_key"
+  type  = "String"
   value = aws_iam_access_key.external_secrets.secret
 }
 
 resource "aws_ssm_parameter" "external_dns_aws_access_key_id" {
-  name  = "/external_dns/aws_access_key_id"
-  type  = "SecureString"
+  name  = "/iam/external_dns/aws_access_key_id"
+  type  = "String"
   value = aws_iam_access_key.external_dns.id
 }
 
 resource "aws_ssm_parameter" "external_dns_aws_secret_access_key" {
-  name  = "/external_dns/aws_secret_access_key"
-  type  = "SecureString"
+  name  = "/iam/external_dns/aws_secret_access_key"
+  type  = "String"
   value = aws_iam_access_key.external_dns.secret
 }
 
 resource "aws_ssm_parameter" "cert_manager_aws_access_key_id" {
-  name  = "/cert_manager/aws_access_key_id"
-  type  = "SecureString"
+  name  = "/iam/cert_manager/aws_access_key_id"
+  type  = "String"
   value = aws_iam_access_key.cert_manager.id
 }
 
 resource "aws_ssm_parameter" "cert_manager_aws_secret_access_key" {
-  name  = "/cert_manager/aws_secret_access_key"
-  type  = "SecureString"
+  name  = "/iam/cert_manager/aws_secret_access_key"
+  type  = "String"
   value = aws_iam_access_key.cert_manager.secret
 } 
 
 resource "aws_ssm_parameter" "api_aws_access_key_id" {
-  name  = "/api/aws_access_key_id"
-  type  = "SecureString"
+  name  = "/iam/api/aws_access_key_id"
+  type  = "String"
   value = aws_iam_access_key.api.id
 }
 
 resource "aws_ssm_parameter" "api_aws_secret_access_key" {
-  name  = "/api/aws_secret_access_key"
-  type  = "SecureString"
+  name  = "/iam/api/aws_secret_access_key"
+  type  = "String"
   value = aws_iam_access_key.api.secret
 }
