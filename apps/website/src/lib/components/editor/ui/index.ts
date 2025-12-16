9dccc23eb8d22ffc880a7b9ab160e1dc23b7f345
dolsup <8360934+dolsup@users.noreply.github.com>
2025-12-16 05:57:54 +0000

    테마 전환 시 setTheme (#2359)

--- apps/website/src/lib/components/editor/ui/index.ts
+++ apps/website/src/lib/components/editor/ui/index.ts
@@ -1,4 +1,3 @@
 export { default as HorizontalRuler } from './HorizontalRuler.svelte';
-export { default as Loading } from './Loading.svelte';
 export { default as Scrollbar } from './Scrollbar.svelte';
 export { default as VerticalRuler } from './VerticalRuler.svelte';
