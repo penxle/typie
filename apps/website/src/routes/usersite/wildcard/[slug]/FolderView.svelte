8a361f9c390fffac9f6d0c79aa4625e040440a46
devunt <337728+devunt@users.noreply.github.com>
2026-01-20 06:29:26 +0000

    folderview에서도 부제목 표시 (#2527)

--- apps/website/src/routes/usersite/wildcard/[slug]/FolderView.svelte
+++ apps/website/src/routes/usersite/wildcard/[slug]/FolderView.svelte
@@ -68,6 +68,7 @@
             ... on PostView {
               id
               title
+              subtitle
               excerpt
               updatedAt
               thumbnail {
@@ -278,6 +279,21 @@
                     >
                       <span class="post-title">{entity.node.title}</span>
                     </h2>
+
+                    {#if entity.node.subtitle}
+                      <h3
+                        class={css({
+                          marginTop: '2px',
+                          fontSize: '14px',
+                          fontWeight: 'medium',
+                          lineHeight: '[1.5]',
+                          color: 'text.subtle',
+                          lineClamp: '2',
+                        })}
+                      >
+                        {entity.node.subtitle}
+                      </h3>
+                    {/if}
 
                     {#if entity.node.excerpt}
                       <p
