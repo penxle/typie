86734522c081b1f6b5b0497cebe099b44a497eee
dolsup <8360934+dolsup@users.noreply.github.com>
2026-02-04 12:23:57 +0000

    전체 선택 후 입력 시 무시되는 버그 수정 (#2675)

--- crates/editor/src/runtime/handlers/input.rs
+++ crates/editor/src/runtime/handlers/input.rs
@@ -6,10 +6,13 @@ impl Runtime {
             return effects;
         }
 
-        self.transact(|tr| {
-            tr.delete_selection()?;
-            tr.insert_text(text)
-        })
+        // NOTE: delete 후 transaction commit 단계에서 trailing paragraph 생성 후 normalize_selection이 되어야 하기 때문에 transact 나눠서 실행
+        let delete_effects = self.transact(|tr| tr.delete_selection());
+        let insert_effects = self.transact(|tr| tr.insert_text(text));
+
+        let mut effects = delete_effects;
+        effects.extend(insert_effects);
+        effects
     }
 
     pub(crate) fn handle_replace_backward(&mut self, length: usize, text: &str) -> Vec<Effect> {
