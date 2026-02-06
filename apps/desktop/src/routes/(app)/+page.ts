16ed5485af019925998397dcdfda448cffadb451
devunt <337728+devunt@users.noreply.github.com>
2026-02-06 07:58:31 +0000

    데스크탑 관련 obsolete된 파일 제거 (#2700)

--- apps/desktop/src/routes/(app)/+page.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import type { App_Query_AfterLoad } from './$graphql';
-
-export const _App_Query_AfterLoad: App_Query_AfterLoad = async ({ query }) => {
-  if (!query.me) {
-    redirect(302, '/auth/login');
-  }
-};
