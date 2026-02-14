777031e8ac952b5f6e60b975bfe5c901f443d49e
Bae Junehyeon <devunt@gmail.com>
2026-02-15 00:20:53 +0900

    slate 코드 중복 매크로로 제거

--- crates/editor/src/runtime/slate.rs
+++ crates/editor/src/runtime/slate.rs
@@ -2,13 +2,12 @@ use crate::layout::elements::ExternalElementData;
 use crate::model::{
     Annotation, AnnotationType, LayoutMode, NodeId, NodeType, Style, StyleType, TextAlign,
 };
+use crate::runtime::tracked_items::TrackedItemOverlay;
+use crate::runtime::{ExternalElement, LinkOverlay, SelectionHandleBounds, TableOverlay};
 use crate::state::selection_helpers::{BlockAttr, SelectionAttributes};
 use crate::state::{Position, Selection};
 use crate::types::{Affinity, PointerStyle, Rect, TextBound};
 use rustc_hash::FxHashMap;
-
-use crate::runtime::tracked_items::TrackedItemOverlay;
-use crate::runtime::{ExternalElement, LinkOverlay, SelectionHandleBounds, TableOverlay};
 
 pub const DIRTY_SETTINGS: u64 = 1 << 0;
 pub const DIRTY_PAGES: u64 = 1 << 1;
@@ -93,90 +92,114 @@ pub fn selection_type(node_type: NodeType) -> u32 {
     }
 }
 
-#[repr(C)]
-pub struct Slate {
-    pub dirty: u64,
+macro_rules! define_slate {
+    (
+        $(#[$meta:meta])*
+        pub struct $name:ident {
+            $(pub $field:ident : $ty:ty),* $(,)?
+        }
+    ) => {
+        $(#[$meta])*
+        pub struct $name {
+            $(pub $field: $ty),*
+        }
 
-    pub slab_len: u32,
-    pub slab_capacity: u32,
+        pub fn get_slate_offsets() -> Vec<(&'static str, usize)> {
+            let mut offsets = vec![
+                $((stringify!($field), std::mem::offset_of!($name, $field))),*
+            ];
+            offsets.sort_by_key(|&(_, off)| off);
+            offsets
+        }
+    };
+}
 
-    pub layout_mode_offset: u32,
+define_slate! {
+    #[repr(C)]
+    pub struct Slate {
+        pub dirty: u64,
 
-    pub paragraph_indent: f32,
-    pub block_gap: f32,
+        pub slab_len: u32,
+        pub slab_capacity: u32,
 
-    pub pages_offset: u32,
-    pub pages_count: u32,
+        pub layout_mode_offset: u32,
 
-    pub cursor_page_idx: i32,
-    pub cursor_x: f32,
-    pub cursor_y: f32,
-    pub cursor_width: f32,
-    pub cursor_height: f32,
-    pub cursor_visible: u32,
+        pub paragraph_indent: f32,
+        pub block_gap: f32,
 
-    pub preceding_char_widths_offset: u32,
-    pub preceding_char_widths_count: u32,
+        pub pages_offset: u32,
+        pub pages_count: u32,
 
-    pub selection_cmp: i32,
-    pub selection_block_ids_offset: u32,
-    pub selection_block_ids_count: u32,
-    pub selection_block_types_offset: u32,
-    pub selection_block_types_count: u32,
-    pub selection_common_ancestor_ids_offset: u32,
-    pub selection_common_ancestor_ids_count: u32,
-    pub selection_common_ancestor_types_offset: u32,
-    pub selection_common_ancestor_types_count: u32,
+        pub cursor_page_idx: i32,
+        pub cursor_x: f32,
+        pub cursor_y: f32,
+        pub cursor_width: f32,
+        pub cursor_height: f32,
+        pub cursor_visible: u32,
 
-    pub selection_anchor_node_id: [u8; 16],
-    pub selection_anchor_offset: u32,
-    pub selection_anchor_affinity: u32,
-    pub selection_anchor_page_idx: i32,
-    pub selection_anchor_x: f32,
-    pub selection_anchor_y: f32,
-    pub selection_anchor_width: f32,
-    pub selection_anchor_height: f32,
+        pub preceding_char_widths_offset: u32,
+        pub preceding_char_widths_count: u32,
 
-    pub selection_head_node_id: [u8; 16],
-    pub selection_head_offset: u32,
-    pub selection_head_affinity: u32,
-    pub selection_head_page_idx: i32,
-    pub selection_head_x: f32,
-    pub selection_head_y: f32,
-    pub selection_head_width: f32,
-    pub selection_head_height: f32,
+        pub selection_cmp: i32,
+        pub selection_block_ids_offset: u32,
+        pub selection_block_ids_count: u32,
+        pub selection_block_types_offset: u32,
+        pub selection_block_types_count: u32,
+        pub selection_common_ancestor_ids_offset: u32,
+        pub selection_common_ancestor_ids_count: u32,
+        pub selection_common_ancestor_types_offset: u32,
+        pub selection_common_ancestor_types_count: u32,
 
-    pub pointer_style: u32,
-    pub pointer_state: u32,
+        pub selection_anchor_node_id: [u8; 16],
+        pub selection_anchor_offset: u32,
+        pub selection_anchor_affinity: u32,
+        pub selection_anchor_page_idx: i32,
+        pub selection_anchor_x: f32,
+        pub selection_anchor_y: f32,
+        pub selection_anchor_width: f32,
+        pub selection_anchor_height: f32,
 
-    pub placeholder_x: f32,
-    pub placeholder_y: f32,
-    pub placeholder_width: f32,
-    pub placeholder_height: f32,
-    pub placeholder_visible: u32,
+        pub selection_head_node_id: [u8; 16],
+        pub selection_head_offset: u32,
+        pub selection_head_affinity: u32,
+        pub selection_head_page_idx: i32,
+        pub selection_head_x: f32,
+        pub selection_head_y: f32,
+        pub selection_head_width: f32,
+        pub selection_head_height: f32,
 
-    pub attrs_offset: u32,
-    pub attrs_count: u32,
+        pub pointer_style: u32,
+        pub pointer_state: u32,
 
-    pub enabled_actions_offset: u32,
-    pub enabled_actions_count: u32,
+        pub placeholder_x: f32,
+        pub placeholder_y: f32,
+        pub placeholder_width: f32,
+        pub placeholder_height: f32,
+        pub placeholder_visible: u32,
 
-    pub external_elements_offset: u32,
-    pub external_elements_count: u32,
+        pub attrs_offset: u32,
+        pub attrs_count: u32,
 
-    pub link_overlays_offset: u32,
-    pub link_overlays_count: u32,
-    pub tracked_items_offset: u32,
-    pub tracked_items_count: u32,
-    pub table_overlays_offset: u32,
-    pub table_overlays_count: u32,
+        pub enabled_actions_offset: u32,
+        pub enabled_actions_count: u32,
 
-    pub font_requests_offset: u32,
-    pub font_requests_count: u32,
-    pub fallback_codepoints_offset: u32,
-    pub fallback_codepoints_count: u32,
-    pub html_pasted_offset: u32,
-    pub html_pasted_len: u32,
+        pub external_elements_offset: u32,
+        pub external_elements_count: u32,
+
+        pub link_overlays_offset: u32,
+        pub link_overlays_count: u32,
+        pub tracked_items_offset: u32,
+        pub tracked_items_count: u32,
+        pub table_overlays_offset: u32,
+        pub table_overlays_count: u32,
+
+        pub font_requests_offset: u32,
+        pub font_requests_count: u32,
+        pub fallback_codepoints_offset: u32,
+        pub fallback_codepoints_count: u32,
+        pub html_pasted_offset: u32,
+        pub html_pasted_len: u32,
+    }
 }
 
 impl Default for Slate {
@@ -828,224 +851,4 @@ fn annotation_type_to_tag(at: &AnnotationType) -> u32 {
         AnnotationType::Link => ATTR_TAG_LINK,
         AnnotationType::Ruby => ATTR_TAG_RUBY,
     }
-}
-
-pub fn get_slate_offsets() -> Vec<(&'static str, usize)> {
-    let mut offsets = vec![
-        ("dirty", std::mem::offset_of!(Slate, dirty)),
-        ("slab_len", std::mem::offset_of!(Slate, slab_len)),
-        ("slab_capacity", std::mem::offset_of!(Slate, slab_capacity)),
-        (
-            "paragraph_indent",
-            std::mem::offset_of!(Slate, paragraph_indent),
-        ),
-        ("block_gap", std::mem::offset_of!(Slate, block_gap)),
-        ("pages_offset", std::mem::offset_of!(Slate, pages_offset)),
-        ("pages_count", std::mem::offset_of!(Slate, pages_count)),
-        (
-            "layout_mode_offset",
-            std::mem::offset_of!(Slate, layout_mode_offset),
-        ),
-        (
-            "cursor_page_idx",
-            std::mem::offset_of!(Slate, cursor_page_idx),
-        ),
-        ("cursor_x", std::mem::offset_of!(Slate, cursor_x)),
-        ("cursor_y", std::mem::offset_of!(Slate, cursor_y)),
-        ("cursor_width", std::mem::offset_of!(Slate, cursor_width)),
-        ("cursor_height", std::mem::offset_of!(Slate, cursor_height)),
-        (
-            "cursor_visible",
-            std::mem::offset_of!(Slate, cursor_visible),
-        ),
-        (
-            "preceding_char_widths_offset",
-            std::mem::offset_of!(Slate, preceding_char_widths_offset),
-        ),
-        (
-            "preceding_char_widths_count",
-            std::mem::offset_of!(Slate, preceding_char_widths_count),
-        ),
-        ("selection_cmp", std::mem::offset_of!(Slate, selection_cmp)),
-        (
-            "selection_block_ids_offset",
-            std::mem::offset_of!(Slate, selection_block_ids_offset),
-        ),
-        (
-            "selection_block_ids_count",
-            std::mem::offset_of!(Slate, selection_block_ids_count),
-        ),
-        (
-            "selection_block_types_offset",
-            std::mem::offset_of!(Slate, selection_block_types_offset),
-        ),
-        (
-            "selection_block_types_count",
-            std::mem::offset_of!(Slate, selection_block_types_count),
-        ),
-        (
-            "selection_common_ancestor_ids_offset",
-            std::mem::offset_of!(Slate, selection_common_ancestor_ids_offset),
-        ),
-        (
-            "selection_common_ancestor_ids_count",
-            std::mem::offset_of!(Slate, selection_common_ancestor_ids_count),
-        ),
-        (
-            "selection_common_ancestor_types_offset",
-            std::mem::offset_of!(Slate, selection_common_ancestor_types_offset),
-        ),
-        (
-            "selection_common_ancestor_types_count",
-            std::mem::offset_of!(Slate, selection_common_ancestor_types_count),
-        ),
-        (
-            "selection_anchor_node_id",
-            std::mem::offset_of!(Slate, selection_anchor_node_id),
-        ),
-        (
-            "selection_anchor_offset",
-            std::mem::offset_of!(Slate, selection_anchor_offset),
-        ),
-        (
-            "selection_anchor_affinity",
-            std::mem::offset_of!(Slate, selection_anchor_affinity),
-        ),
-        (
-            "selection_head_node_id",
-            std::mem::offset_of!(Slate, selection_head_node_id),
-        ),
-        (
-            "selection_head_offset",
-            std::mem::offset_of!(Slate, selection_head_offset),
-        ),
-        (
-            "selection_head_affinity",
-            std::mem::offset_of!(Slate, selection_head_affinity),
-        ),
-        (
-            "selection_anchor_page_idx",
-            std::mem::offset_of!(Slate, selection_anchor_page_idx),
-        ),
-        (
-            "selection_anchor_x",
-            std::mem::offset_of!(Slate, selection_anchor_x),
-        ),
-        (
-            "selection_anchor_y",
-            std::mem::offset_of!(Slate, selection_anchor_y),
-        ),
-        (
-            "selection_anchor_width",
-            std::mem::offset_of!(Slate, selection_anchor_width),
-        ),
-        (
-            "selection_anchor_height",
-            std::mem::offset_of!(Slate, selection_anchor_height),
-        ),
-        (
-            "selection_head_page_idx",
-            std::mem::offset_of!(Slate, selection_head_page_idx),
-        ),
-        (
-            "selection_head_x",
-            std::mem::offset_of!(Slate, selection_head_x),
-        ),
-        (
-            "selection_head_y",
-            std::mem::offset_of!(Slate, selection_head_y),
-        ),
-        (
-            "selection_head_width",
-            std::mem::offset_of!(Slate, selection_head_width),
-        ),
-        (
-            "selection_head_height",
-            std::mem::offset_of!(Slate, selection_head_height),
-        ),
-        ("attrs_offset", std::mem::offset_of!(Slate, attrs_offset)),
-        ("attrs_count", std::mem::offset_of!(Slate, attrs_count)),
-        ("pointer_style", std::mem::offset_of!(Slate, pointer_style)),
-        ("pointer_state", std::mem::offset_of!(Slate, pointer_state)),
-        (
-            "placeholder_visible",
-            std::mem::offset_of!(Slate, placeholder_visible),
-        ),
-        ("placeholder_x", std::mem::offset_of!(Slate, placeholder_x)),
-        ("placeholder_y", std::mem::offset_of!(Slate, placeholder_y)),
-        (
-            "placeholder_width",
-            std::mem::offset_of!(Slate, placeholder_width),
-        ),
-        (
-            "placeholder_height",
-            std::mem::offset_of!(Slate, placeholder_height),
-        ),
-        (
-            "enabled_actions_offset",
-            std::mem::offset_of!(Slate, enabled_actions_offset),
-        ),
-        (
-            "enabled_actions_count",
-            std::mem::offset_of!(Slate, enabled_actions_count),
-        ),
-        (
-            "external_elements_offset",
-            std::mem::offset_of!(Slate, external_elements_offset),
-        ),
-        (
-            "external_elements_count",
-            std::mem::offset_of!(Slate, external_elements_count),
-        ),
-        (
-            "link_overlays_offset",
-            std::mem::offset_of!(Slate, link_overlays_offset),
-        ),
-        (
-            "link_overlays_count",
-            std::mem::offset_of!(Slate, link_overlays_count),
-        ),
-        (
-            "tracked_items_offset",
-            std::mem::offset_of!(Slate, tracked_items_offset),
-        ),
-        (
-            "tracked_items_count",
-            std::mem::offset_of!(Slate, tracked_items_count),
-        ),
-        (
-            "table_overlays_offset",
-            std::mem::offset_of!(Slate, table_overlays_offset),
-        ),
-        (
-            "table_overlays_count",
-            std::mem::offset_of!(Slate, table_overlays_count),
-        ),
-        (
-            "font_requests_offset",
-            std::mem::offset_of!(Slate, font_requests_offset),
-        ),
-        (
-            "font_requests_count",
-            std::mem::offset_of!(Slate, font_requests_count),
-        ),
-        (
-            "fallback_codepoints_offset",
-            std::mem::offset_of!(Slate, fallback_codepoints_offset),
-        ),
-        (
-            "fallback_codepoints_count",
-            std::mem::offset_of!(Slate, fallback_codepoints_count),
-        ),
-        (
-            "html_pasted_offset",
-            std::mem::offset_of!(Slate, html_pasted_offset),
-        ),
-        (
-            "html_pasted_len",
-            std::mem::offset_of!(Slate, html_pasted_len),
-        ),
-    ];
-    offsets.sort_by_key(|&(_, off)| off);
-    offsets
 }
