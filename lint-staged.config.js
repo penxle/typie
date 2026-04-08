470231d60f174355dcb76ba7103b480559b4e529
devunt <337728+devunt@users.noreply.github.com>
2026-04-08 13:30:30 +0000

    lefthook -> husky + lint-staged (#3713)

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+export default {
+  '*.{ts,tsx,js,jsx,svelte}': ['eslint --fix --quiet', 'prettier --write --ignore-unknown'],
+  '*.{json,yaml,yml,md,graphql}': ['prettier --write --ignore-unknown --no-error-on-unmatched-pattern'],
+  '*': ['cspell --no-progress --relative --no-must-find-files'],
+  '*.dart': (files) => [...files.map((f) => `dart fix --apply "${f}"`), `dart format ${files.join(' ')}`],
+  '*.rs': ['cargo fmt --'],
+};
