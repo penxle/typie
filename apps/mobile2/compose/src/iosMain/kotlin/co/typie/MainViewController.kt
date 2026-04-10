1520568ba5a7981945303c99b265f5ffb892730d
devunt <337728+devunt@users.noreply.github.com>
2026-04-10 18:13:05 +0000

    iOS paralell rendering 비활성화 (#3773)
    composition thread 와 render thread 간 race condition으로 앱 크래시 발생함

--- apps/mobile2/compose/src/iosMain/kotlin/co/typie/MainViewController.kt
+++ apps/mobile2/compose/src/iosMain/kotlin/co/typie/MainViewController.kt
@@ -1,14 +1,10 @@
-@file:OptIn(ExperimentalComposeUiApi::class)
-
 package co.typie
 
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.uikit.OnFocusBehavior
 import androidx.compose.ui.window.ComposeUIViewController
 
 fun MainViewController() = ComposeUIViewController(
   configure = {
     onFocusBehavior = OnFocusBehavior.DoNothing
-    parallelRendering = true
   },
 ) { App() }
