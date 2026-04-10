5820a3641b5ad02c5192849d64ccbc322849effa
devunt <337728+devunt@users.noreply.github.com>
2026-04-10 17:21:32 +0000

    디펜던시 업그레이드 후속 이어서 (#3771)

--- apps/mobile2/compose/src/commonMain/kotlin/co/typie/graphql/ApolloClient.kt
+++ apps/mobile2/compose/src/commonMain/kotlin/co/typie/graphql/ApolloClient.kt
@@ -6,6 +6,7 @@ import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.annotations.ApolloExperimental
 import com.apollographql.apollo.api.Subscription
 import com.apollographql.apollo.network.websocket.GraphQLWsProtocol
+import com.apollographql.apollo.network.websocket.WebSocketEngine
 import com.apollographql.apollo.network.websocket.WebSocketNetworkTransport
 import com.apollographql.cache.normalized.FetchPolicy
 import com.apollographql.cache.normalized.api.CacheKey
@@ -14,17 +15,18 @@ import com.apollographql.cache.normalized.api.IdCacheResolver
 import com.apollographql.cache.normalized.fetchPolicy
 import com.apollographql.cache.normalized.memory.MemoryCacheFactory
 import com.apollographql.cache.normalized.normalizedCache
-import com.apollographql.ktor.ktorClient
+import com.apollographql.ktor.http.KtorHttpEngine
 
 @OptIn(ApolloExperimental::class)
 val Apollo: ApolloClient = ApolloClient.Builder()
   .serverUrl("${Konfig.API_URL}/graphql")
-  .ktorClient(Http)
+  .httpEngine(KtorHttpEngine(Http))
   .fetchPolicy(FetchPolicy.CacheAndNetwork)
   .retryOnError { request -> request.operation is Subscription<*> }
   .subscriptionNetworkTransport(
     WebSocketNetworkTransport.Builder()
       .serverUrl("${Konfig.WS_URL}/graphql")
+      .webSocketEngine(WebSocketEngine())
       .wsProtocol(
         GraphQLWsProtocol(
           connectionPayload = { WebSocketSessionService.createConnectionPayload() },
--- apps/mobile2/compose/src/commonMain/kotlin/co/typie/result/ResultDefaults.kt
+++ apps/mobile2/compose/src/commonMain/kotlin/co/typie/result/ResultDefaults.kt
@@ -1,9 +1,13 @@
 package co.typie.result
 
+import co.touchlab.kermit.Logger
 import co.typie.overlay.Toast
 import co.typie.overlay.ToastType
 
 const val DEFAULT_ERROR_MESSAGE = "오류가 발생했어요. 잠시 후 다시 시도해주세요."
 
 fun <T, E> Result<T, E>.withDefaultExceptionHandler(toast: Toast): Result<T, E> =
-  onException { toast.show(ToastType.Error, DEFAULT_ERROR_MESSAGE) }
+  onException {
+    Logger.e(it) { "Unhandled exception" }
+    toast.show(ToastType.Error, DEFAULT_ERROR_MESSAGE)
+  }
