8776fdec7c981ee4c200e5ecfe517db6d360dbc0
dolsup <8360934+dolsup@users.noreply.github.com>
2026-04-20 13:12:50 +0000

    EntityIconPickerSheet 올바른 safeDrawing을 import해서 사용 (#3956)

--- apps/mobile2/compose/src/commonMain/kotlin/co/typie/domain/entity/icon/EntityIconPickerSheet.kt
+++ apps/mobile2/compose/src/commonMain/kotlin/co/typie/domain/entity/icon/EntityIconPickerSheet.kt
@@ -8,14 +8,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -36,6 +33,7 @@ import androidx.compose.ui.unit.dp
 import co.typie.ext.InteractionScope
 import co.typie.ext.clickable
 import co.typie.ext.pressScale
+import co.typie.ext.safeDrawing
 import co.typie.form.FormState
 import co.typie.icons.Lucide
 import co.typie.result.Result
@@ -211,11 +209,7 @@ internal fun EntityIconPickerSheet(
             .weight(1f)
             .scrollFog(padding = gridFogInsets, color = AppTheme.colors.surfaceCanvas)
       ) {
-        val safeBottom =
-          WindowInsets.safeDrawing
-            .only(WindowInsetsSides.Bottom)
-            .asPaddingValues()
-            .calculateBottomPadding()
+        val safeBottom = WindowInsets.safeDrawing.asPaddingValues().calculateBottomPadding()
 
         LazyVerticalGrid(
           columns = GridCells.Fixed(7),
