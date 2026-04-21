b5e50fd0078bd4a3fa591b39068ab5cb48a5a921
devunt <337728+devunt@users.noreply.github.com>
2026-04-21 08:44:58 +0000

    모바일 앱 폰트 최적화 (#3990)
    16MB -> 4.3MB

--- apps/mobile/compose/src/commonMain/kotlin/co/typie/ui/theme/Typography.kt
+++ apps/mobile/compose/src/commonMain/kotlin/co/typie/ui/theme/Typography.kt
@@ -6,54 +6,34 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import co.typie.generated.resources.Res
-import co.typie.generated.resources.paperlogy_black
 import co.typie.generated.resources.paperlogy_bold
-import co.typie.generated.resources.paperlogy_extrabold
-import co.typie.generated.resources.paperlogy_extralight
-import co.typie.generated.resources.paperlogy_light
 import co.typie.generated.resources.paperlogy_medium
 import co.typie.generated.resources.paperlogy_regular
 import co.typie.generated.resources.paperlogy_semibold
-import co.typie.generated.resources.paperlogy_thin
 import co.typie.generated.resources.suit_bold
-import co.typie.generated.resources.suit_extrabold
-import co.typie.generated.resources.suit_extralight
-import co.typie.generated.resources.suit_heavy
-import co.typie.generated.resources.suit_light
 import co.typie.generated.resources.suit_medium
 import co.typie.generated.resources.suit_regular
 import co.typie.generated.resources.suit_semibold
-import co.typie.generated.resources.suit_thin
 import org.jetbrains.compose.resources.Font
 
 val SuitFontFamily: FontFamily
   @Composable
   get() =
     FontFamily(
-      Font(Res.font.suit_thin, FontWeight.Thin),
-      Font(Res.font.suit_extralight, FontWeight.ExtraLight),
-      Font(Res.font.suit_light, FontWeight.Light),
       Font(Res.font.suit_regular, FontWeight.Normal),
       Font(Res.font.suit_medium, FontWeight.Medium),
       Font(Res.font.suit_semibold, FontWeight.SemiBold),
       Font(Res.font.suit_bold, FontWeight.Bold),
-      Font(Res.font.suit_extrabold, FontWeight.ExtraBold),
-      Font(Res.font.suit_heavy, FontWeight.Black),
     )
 
 val PaperlogyFontFamily: FontFamily
   @Composable
   get() =
     FontFamily(
-      Font(Res.font.paperlogy_thin, FontWeight.Thin),
-      Font(Res.font.paperlogy_extralight, FontWeight.ExtraLight),
-      Font(Res.font.paperlogy_light, FontWeight.Light),
       Font(Res.font.paperlogy_regular, FontWeight.Normal),
       Font(Res.font.paperlogy_medium, FontWeight.Medium),
       Font(Res.font.paperlogy_semibold, FontWeight.SemiBold),
       Font(Res.font.paperlogy_bold, FontWeight.Bold),
-      Font(Res.font.paperlogy_extrabold, FontWeight.ExtraBold),
-      Font(Res.font.paperlogy_black, FontWeight.Black),
     )
 
 object AppTypography {
