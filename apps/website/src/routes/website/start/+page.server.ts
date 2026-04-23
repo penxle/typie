7c80017c5e8ae14d082e0275dbbfd0d6a55840d0
devunt <337728+devunt@users.noreply.github.com>
2026-04-23 15:24:58 +0000

    server route -> page route (#4012)

--- apps/website/src/routes/website/start/+page.server.ts
+++ apps/website/src/routes/website/start/+page.server.ts
@@ -1,9 +1,9 @@
 import { redirect } from '@sveltejs/kit';
 import { env } from '$env/dynamic/public';
 import { resolveAppStoreUrl } from '$lib/server/app-redirect';
-import type { RequestHandler } from './$types';
+import type { PageServerLoad } from './$types';
 
-export const GET: RequestHandler = async ({ request }) => {
+export const load: PageServerLoad = async ({ request }) => {
   const userAgent = request.headers.get('user-agent');
   redirect(302, resolveAppStoreUrl(userAgent, env.PUBLIC_AUTH_URL));
 };
