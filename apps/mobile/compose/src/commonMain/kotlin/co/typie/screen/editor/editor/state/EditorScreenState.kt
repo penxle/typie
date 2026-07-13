17d1951f1f84ffb199b4578e8b51e24f111878fe
dolsup <8360934+dolsup@users.noreply.github.com>
2026-07-13 08:53:40 +0000

    add missing changes (#4788)

--- apps/mobile/compose/src/commonMain/kotlin/co/typie/screen/editor/editor/state/EditorScreenState.kt
+++ apps/mobile/compose/src/commonMain/kotlin/co/typie/screen/editor/editor/state/EditorScreenState.kt
@@ -12,7 +12,7 @@ import co.typie.editor.ffi.SystemEvent
 import co.typie.editor.runtime.EditorRuntime
 import co.typie.editor.runtime.EditorUiState
 import co.typie.editor.scroll.EditorVisibleArea
-import co.typie.editor.sync.ActiveSyncEngines
+import co.typie.editor.sync.ActiveDocumentEditingSessions
 import co.typie.editor.sync.catchingNonCancellation
 import co.typie.editor.viewport.EditorViewportState
 import kotlin.math.max
@@ -58,7 +58,7 @@ internal class EditorScreenState internal constructor(val viewportState: EditorV
     runtime.editor?.enqueue(Message.System(SystemEvent.SetFocused(false)))
     runtime.deactivateScene()
     flushDrafts()
-    catchingNonCancellation { ActiveSyncEngines.flushAll() }
+    catchingNonCancellation { ActiveDocumentEditingSessions.flushSyncAll() }
     withFrameNanos {}
   }
 
